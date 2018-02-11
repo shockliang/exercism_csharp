7cfcec3a75bfcc634ebcd1621b1be94008a0fffd
Shock Liang <shockwater@gmail.com>
2018-02-11 15:41:40 +0800

    Submit sum of multiples solution.

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var allResult = new List<int>();
+
+        foreach (var item in multiples)
+        {
+            if (item > max)
+                continue;
+
+            var multipleResult = 0;
+
+            var count = 1;
+            while (multipleResult < max)
+            {
+                multipleResult = item * count;
+                count++;
+                if (multipleResult < max)
+                    allResult.Add(multipleResult);
+            }
+        }
+
+        return allResult.Distinct().Sum();
     }
 }
