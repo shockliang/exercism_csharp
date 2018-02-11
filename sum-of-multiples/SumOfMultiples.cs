9796389ba0aa5614fea545a48d340744e3362047
Shock Liang <shockwater@gmail.com>
2018-02-11 15:59:05 +0800

    Sum of multiples solution version 2

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -6,25 +6,6 @@ public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        var allResult = new List<int>();
-
-        foreach (var item in multiples)
-        {
-            if (item > max)
-                continue;
-
-            var multipleResult = 0;
-
-            var count = 1;
-            while (multipleResult < max)
-            {
-                multipleResult = item * count;
-                count++;
-                if (multipleResult < max)
-                    allResult.Add(multipleResult);
-            }
-        }
-
-        return allResult.Distinct().Sum();
+        return Enumerable.Range(1, max-1).Where(n => multiples.Any(m => n % m == 0)).Sum();
     }
 }
