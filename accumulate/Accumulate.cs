62c944c20a594603dc27f9b018d2b90a696153bb
Shock Liang <shockwater@gmail.com>
2018-02-13 19:49:55 +0800

    Initial accumlate project.

---
+++ accumulate/Accumulate.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class AccumulateExtensions
+{
+    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
