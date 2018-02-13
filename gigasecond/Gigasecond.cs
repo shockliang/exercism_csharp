5770b1a7c3b4d849f1160b70cd40f2c6bafdaf32
Shock Liang <shockwater@gmail.com>
2018-02-13 19:18:43 +0800

    Submit gigasecond solution version 2.

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -2,9 +2,5 @@ using System;
 
 public static class Gigasecond
 {
-    public static readonly double SECONDS = 1000000000.0d;
-    public static DateTime Add(DateTime birthDate)
-    {
-        return birthDate.AddSeconds(SECONDS);
-    }
+    public static DateTime Add(DateTime birthDate) => birthDate.AddSeconds(1E9);
 }
