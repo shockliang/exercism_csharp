96e1df56ccfbf4d907299fb68ae2a061db65c7d5
Shock Liang <shockwater@gmail.com>
2018-02-13 22:43:23 +0800

    Submit accumulate solution version 2.

--- accumulate/Accumulate.cs
+++ accumulate/Accumulate.cs
@@ -6,9 +6,6 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        foreach (var item in collection)
-        {
-            yield return func(item);
-        }
+        return collection.Select(func);
     }
 }
