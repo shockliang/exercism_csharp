062fd151668ff6e3de81b077c04191c11fdc1b5e
Shock Liang <shockwater@gmail.com>
2018-02-14 20:44:05 +0800

    Initial nucleotide count project.

---
+++ nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+public class NucleotideCount
+{
+    public NucleotideCount(string sequence)
+    {
+    }
+
+    public IDictionary<char, int> NucleotideCounts
+    {
+        get
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
+
+public class InvalidNucleotideException : Exception { }
