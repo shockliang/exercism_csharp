f5cf8d222174e844973c26f3a2f6b8784f073210
Shock Liang <shockwater@gmail.com>
2018-02-16 16:46:48 +0800

    Submit collatz conjecture solution version 2.

--- collatz-conjecture/CollatzConjecture.cs
+++ collatz-conjecture/CollatzConjecture.cs
@@ -4,17 +4,8 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        if (number < 1)
-            throw new ArgumentException();
-            
-        var steps = 0;
-
-        while (number > 1)
-        {
-            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
-            steps++;
-        }
-
-        return steps;
+        if (number < 1) throw new ArgumentException();
+        if (number == 1) return 0;
+        return 1 + Steps(number % 2 == 0 ? number / 2 : number * 3 + 1);
     }
 }
