b9fe4f911bc7297b2a10493901d92e31af6140ab
Shock Liang <shockwater@gmail.com>
2018-02-17 19:15:50 +0800

    Submit armstrong number solution version 1.

--- armstrong-numbers/ArmstrongNumbers.cs
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var copyNumber = number;
+        var power = number.ToString().Length;
+        var sum = 0d;
+        
+        while (copyNumber > 0)
+        {
+            sum += Math.Pow(copyNumber % 10, power);
+            copyNumber /= 10;
+        }
+
+        return sum == number;
     }
 }
