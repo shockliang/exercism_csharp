d86113846526c4a94afcfe953c1e69065352ab76
Shock Liang <shockwater@gmail.com>
2018-02-18 18:13:08 +0800

    Submit rna transcription solution version 1.

--- rna-transcription/RnaTranscription.cs
+++ rna-transcription/RnaTranscription.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class RnaTranscription
 {
+    private static readonly IDictionary<char, char> mapping = new Dictionary<char, char>()
+    {
+        ['G'] = 'C',
+        ['C'] = 'G',
+        ['T'] = 'A',
+        ['A'] = 'U'
+    };
+    
     public static string ToRna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new string(nucleotide.ToUpper().Select(c => mapping[c]).ToArray());
     }
 }
