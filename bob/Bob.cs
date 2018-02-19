6c1375359c01d9287274269b0a405efe2ca56f7e
Shock Liang <shockwater@gmail.com>
2018-02-19 19:33:23 +0800

    Submit bob solution version 1.

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class Bob
 {
+    private static Regex lettersRegex = new Regex(@"[a-zA-Z]");
+    private static Regex numberRegex = new Regex(@"[0-9]");
+
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        statement = statement.TrimEnd();
+
+        if (IsQuestion(statement))
+            if (IsYell(statement) && IsLetters(statement))
+                return "Calm down, I know what I'm doing!";
+            else
+                return "Sure.";
+        else if (IsYell(statement))
+            return "Whoa, chill out!";
+        else if (IsSilence(statement))
+            return "Fine. Be that way!";
+        else
+            return "Whatever.";
+    }
+
+    private static bool IsQuestion(string statement)
+    {
+        return statement.EndsWith("?");
+    }
+
+    private static bool IsYell(string statement)
+    {
+        return statement == statement.ToUpper() && !IsSilence(statement) && !IsOnlyNumber(statement);
+    }
+
+    private static bool IsSilence(string statement)
+    {
+        return string.IsNullOrEmpty(statement) || string.IsNullOrWhiteSpace(statement);
+    }
+
+    private static bool IsLetters(string statement)
+    {
+        return lettersRegex.IsMatch(statement);
+    }
+
+    private static bool IsOnlyNumber(string statement)
+    {
+        return numberRegex.IsMatch(statement) && !lettersRegex.IsMatch(statement);
     }
 }
