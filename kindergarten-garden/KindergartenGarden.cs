09aeccfbb50c35782750caed0cbc902fdc3b44fb
Shock Liang <shockwater@gmail.com>
2018-02-20 19:14:25 +0800

    Submit kindergarten garden solution version 1.

--- kindergarten-garden/KindergartenGarden.cs
+++ kindergarten-garden/KindergartenGarden.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public enum Plant
@@ -10,9 +11,28 @@ public enum Plant
 }
 
 public class KindergartenGarden
-{    
+{
+    private List<string> students = new List<string>(new string[]
+    {
+        "Alice", "Bob", "Charlie", "David",
+        "Eve", "Fred", "Ginny", "Harriet",
+        "Ileana", "Joseph", "Kincaid", "Larry"
+    }).OrderBy(x => x).ToList();
+
+    private Garden[] gardens;
+
     public KindergartenGarden(string diagram)
     {
+        var plantBucket = diagram.Split("\n");
+        var holderCount = plantBucket.FirstOrDefault().Count() / 2;
+        gardens = new Garden[students.Count];
+
+        for (int i = 0; i < holderCount; i++)
+        {
+            var plants = plantBucket[0].Skip(i * 2).Take(2).ToArray().Concat(plantBucket[1].Skip(i * 2).Take(2)).ToArray();
+            gardens[i] = new Garden(students[i], plants);
+        }
+
     }
 
     public KindergartenGarden(string diagram, IEnumerable<string> students)
@@ -21,6 +41,32 @@ public class KindergartenGarden
 
     public IEnumerable<Plant> Plants(string student)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return gardens.Where(x => x.Holer.Equals(student)).FirstOrDefault().Plants;
+    }
+}
+
+internal class Garden
+{
+    private static readonly IDictionary<char, Plant> mapping = new Dictionary<char, Plant>()
+    {
+        ['V'] = Plant.Violets,
+        ['R'] = Plant.Radishes,
+        ['C'] = Plant.Clover,
+        ['G'] = Plant.Grass,
+    };
+
+    public string Holer { get; }
+
+    public IEnumerable<Plant> Plants { get; }
+
+    public Garden(string holer, IEnumerable<char> plants)
+    {
+        this.Holer = holer;
+        this.Plants = PlantParser(plants);
+    }
+
+    private IEnumerable<Plant> PlantParser(IEnumerable<char> plants)
+    {
+        return plants.Select(x => mapping[x]);
     }
 }
