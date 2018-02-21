75e79e938efa9a71ca371cd114fed922f0bb8e50
Shock Liang <shockwater@gmail.com>
2018-02-21 23:10:54 +0800

    Submit clock solution version 2.

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -1,67 +1,30 @@
 using System;
 
-public class Clock : IEquatable<Clock>
+public struct Clock
 {
     public Clock(int hours, int minutes)
     {
-        Hours = HoursParser(hours, minutes);
-        Minutes = MinutesParser(minutes);
-    }
+        Hours = hours + Math.DivRem(minutes, 60, out int m);
+        Minutes = m;
 
-    public int Hours { get; private set; }
-
-    public int Minutes { get; private set; }
-
-    public Clock Add(int minutesToAdd)
-    {
-        Hours = HoursParser(Hours, Minutes + minutesToAdd);
-        Minutes = MinutesParser(Minutes + minutesToAdd);
-        return this;
-    }
-
-    public bool Equals(Clock other)
-    {
-        return this.ToString().Equals(other.ToString());
-    }
-
-    public Clock Subtract(int minutesToSubtract)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public override string ToString()
-    {
-        return $"{Hours.ToString("D2")}:{Minutes.ToString("D2")}";
-    }
-
-    private int HoursParser(int hours, int minutes)
-    {
-        if (minutes >= 60)
+        if (Minutes < 0)
         {
-            hours += minutes / 60;
-        }
-        else if (minutes < 0)
-        {
-            hours -= (Math.Abs((minutes / 60)) + 1);
+            Minutes += 60;
+            Hours--;
         }
 
-        if (hours >= 0)
-        {
-            hours %= 24;
-        }
-        else
-        {
-            if (hours % 24 == 0)
-                hours = 0;
-            else
-                hours = 24 + (hours % 24);
-        }
+        Hours %= 24;
 
-        return hours;
+        if (Hours < 0) Hours += 24;
     }
 
-    private int MinutesParser(int minutes)
-    {
-        return minutes >= 0 ? minutes % 60 : 60 + (minutes % 60);
-    }
+    public int Hours { get; }
+
+    public int Minutes { get; }
+
+    public Clock Add(int minutesToAdd) => new Clock(Hours, Minutes + minutesToAdd);
+
+    public Clock Subtract(int minutesToSubtract) => Add(-minutesToSubtract);
+
+    public override string ToString() => $"{Hours:00}:{Minutes:00}";
 }
