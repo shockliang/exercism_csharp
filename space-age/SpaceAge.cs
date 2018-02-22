526fb24580230fb5b4aec4393a2bf6b7b56bf47b
Shock Liang <shockwater@gmail.com>
2018-02-23 00:45:54 +0800

    Submit space age solution version 2.

--- space-age/SpaceAge.cs
+++ space-age/SpaceAge.cs
@@ -2,61 +2,27 @@ using System;
 
 public class SpaceAge
 {
-    private static readonly double EARTH_YEAR = 31557600d;
-    private static readonly double MERCURY_YEAR = EARTH_YEAR * 0.2408467d;
-    private static readonly double VENUS_YEAR = EARTH_YEAR * 0.61519726d;
-    private static readonly double MARS_YEAR = EARTH_YEAR * 1.8808158d;
-    private static readonly double JUPITER_YEAR = EARTH_YEAR * 11.862615d;
-    private static readonly double SATURN_YEAR = EARTH_YEAR * 29.447498d;
-    private static readonly double URANUS_YEAR = EARTH_YEAR * 84.016846d;
-    private static readonly double NEPTUNE_YEAR = EARTH_YEAR * 164.79132d;
-
     private readonly double seconds;
+    private readonly double earthYear = 31557600.0;
 
-    private readonly int roundDigits = 2;
+    private double CalcAndRound(double rate)
+        => Math.Round(seconds / earthYear / rate, 2, MidpointRounding.AwayFromZero);
 
-    public SpaceAge(long seconds)
-    {
-        this.seconds = (double)seconds;
-    }
+    public SpaceAge(long seconds) => this.seconds = seconds;
 
-    public double OnEarth()
-    {
-        return Math.Round(seconds / EARTH_YEAR, roundDigits);
-    }
+    public double OnEarth() => CalcAndRound(1);
 
-    public double OnMercury()
-    {
-        return Math.Round(seconds / MERCURY_YEAR, roundDigits);
-    }
+    public double OnMercury() => CalcAndRound(0.2408467);
 
-    public double OnVenus()
-    {
-        return Math.Round(seconds / VENUS_YEAR, roundDigits);
-    }
+    public double OnVenus() => CalcAndRound(0.61519726);
 
-    public double OnMars()
-    {
-        return Math.Round(seconds / MARS_YEAR, roundDigits);
-    }
+    public double OnMars() => CalcAndRound(1.8808158);
 
-    public double OnJupiter()
-    {
-        return Math.Round(seconds / JUPITER_YEAR, roundDigits);
-    }
+    public double OnJupiter() => CalcAndRound(11.862615);
 
-    public double OnSaturn()
-    {
-        return Math.Round(seconds / SATURN_YEAR, roundDigits);
-    }
+    public double OnSaturn() => CalcAndRound(29.447498);
 
-    public double OnUranus()
-    {
-        return Math.Round(seconds / URANUS_YEAR, roundDigits);
-    }
+    public double OnUranus() => CalcAndRound(84.016846);
 
-    public double OnNeptune()
-    {
-        return Math.Round(seconds / NEPTUNE_YEAR, roundDigits);
-    }
+    public double OnNeptune() => CalcAndRound(164.79132);
 }
