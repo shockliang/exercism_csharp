791f289450df30c7034d7a6908a3772cde790298
Shock Liang <shockwater@gmail.com>
2018-02-23 00:38:58 +0800

    Submit space age solution version 1.

--- space-age/SpaceAge.cs
+++ space-age/SpaceAge.cs
@@ -2,47 +2,61 @@ using System;
 
 public class SpaceAge
 {
+    private static readonly double EARTH_YEAR = 31557600d;
+    private static readonly double MERCURY_YEAR = EARTH_YEAR * 0.2408467d;
+    private static readonly double VENUS_YEAR = EARTH_YEAR * 0.61519726d;
+    private static readonly double MARS_YEAR = EARTH_YEAR * 1.8808158d;
+    private static readonly double JUPITER_YEAR = EARTH_YEAR * 11.862615d;
+    private static readonly double SATURN_YEAR = EARTH_YEAR * 29.447498d;
+    private static readonly double URANUS_YEAR = EARTH_YEAR * 84.016846d;
+    private static readonly double NEPTUNE_YEAR = EARTH_YEAR * 164.79132d;
+
+    private readonly double seconds;
+
+    private readonly int roundDigits = 2;
+
     public SpaceAge(long seconds)
     {
+        this.seconds = (double)seconds;
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(seconds / EARTH_YEAR, roundDigits);
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(seconds / MERCURY_YEAR, roundDigits);
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(seconds / VENUS_YEAR, roundDigits);
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(seconds / MARS_YEAR, roundDigits);
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(seconds / JUPITER_YEAR, roundDigits);
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(seconds / SATURN_YEAR, roundDigits);
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(seconds / URANUS_YEAR, roundDigits);
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Round(seconds / NEPTUNE_YEAR, roundDigits);
     }
 }
