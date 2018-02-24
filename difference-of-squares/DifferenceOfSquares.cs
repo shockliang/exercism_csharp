9577042d1b2198b5611d626d99f5f5c456149422
Shock Liang <shockwater@gmail.com>
2018-02-24 17:55:41 +0800

    Submit difference of squares solution version 1.

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 
+/// <summary>
+/// Formula according Gauss. https://brilliant.org/wiki/gauss-the-prince-of-mathematics/
+/// </summary>
 public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Convert.ToInt32(Math.Pow((1 + max) * max / 2, 2));
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Convert.ToInt32((max * (max + 1) * (2 * max + 1)) / 6);
     }
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Math.Abs(CalculateSumOfSquares(max) - CalculateSquareOfSum(max));
     }
 }
