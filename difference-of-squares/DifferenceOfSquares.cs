1cb6ceb85d0608ee5dcce8b1b7fd5bd284d6d8a6
Shock Liang <shockwater@gmail.com>
2018-02-24 18:08:26 +0800

    Submit difference of squares solution version 2 that using linq from @Wonderdch.

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -1,22 +1,16 @@
 ﻿using System;
+using System.Linq;
 
-/// <summary>
-/// Formula according Gauss. https://brilliant.org/wiki/gauss-the-prince-of-mathematics/
-/// </summary>
 public static class DifferenceOfSquares
 {
-    public static int CalculateSquareOfSum(int max)
-    {
-        return Convert.ToInt32(Math.Pow((1 + max) * max / 2, 2));
-    }
+    public static int CalculateSquareOfSum(int max) => Enumerable.Range(1, max).Sum().Square();
 
-    public static int CalculateSumOfSquares(int max)
-    {
-        return Convert.ToInt32((max * (max + 1) * (2 * max + 1)) / 6);
-    }
+    public static int CalculateSumOfSquares(int max) => Enumerable.Range(1, max).Sum(i => i.Square());
 
-    public static int CalculateDifferenceOfSquares(int max)
-    {
-        return Math.Abs(CalculateSumOfSquares(max) - CalculateSquareOfSum(max));
-    }
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+}
+
+public static class ExtendIntSquare
+{
+    public static int Square(this int i) => i * i;
 }
