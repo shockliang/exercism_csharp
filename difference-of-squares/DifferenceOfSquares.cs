78f348ff8a8dbf72a6dd9fc336925d81a2de1885
Shock Liang <shockwater@gmail.com>
2018-02-24 21:10:31 +0800

    Restore.

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -3,10 +3,7 @@ using System.Linq;
 
 public class DifferenceOfSquares
 {
-    public static int CalculateSquareOfSum(int max)
-    {
-        return Enumerable.Range(1, max).Sum().Square();
-    }
+    public static int CalculateSquareOfSum(int max) => Enumerable.Range(1, max).Sum().Square();
 
     public static int CalculateSumOfSquares(int max) => Enumerable.Range(1, max).Sum(i => i.Square());
 
