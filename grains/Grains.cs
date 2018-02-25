1dd5e288a9b322f6669d47947d799b834acea85a
Shock Liang <shockwater@gmail.com>
2018-02-25 16:41:12 +0800

    Submit grains solution version 1.

--- grains/Grains.cs
+++ grains/Grains.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 public static class Grains
 {
     public static ulong Square(int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (n <= 0 || n > 64)
+            throw new ArgumentOutOfRangeException();
+
+        return Convert.ToUInt64(Math.Pow(2, n - 1));
     }
 
     public static ulong Total()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enumerable.Range(1, 64).Select(x => Square(x)).Aggregate((result, item) => result + item);
     }
 }
