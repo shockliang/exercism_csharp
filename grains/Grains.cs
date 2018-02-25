76a3a1f6d1c86fbb826d25e07beff8ecc87ce852
Shock Liang <shockwater@gmail.com>
2018-02-25 16:57:04 +0800

    Submit grains solution version 2.

--- grains/Grains.cs
+++ grains/Grains.cs
@@ -5,14 +5,14 @@ public static class Grains
 {
     public static ulong Square(int n)
     {
-        if (n <= 0 || n > 64)
+        if (n < 1 || n > 64)
             throw new ArgumentOutOfRangeException();
 
-        return Convert.ToUInt64(Math.Pow(2, n - 1));
+        return (ulong)1 << (n - 1);
     }
 
     public static ulong Total()
     {
-        return Enumerable.Range(1, 64).Select(x => Square(x)).Aggregate((result, item) => result + item);
+        return ulong.MaxValue;
     }
 }
