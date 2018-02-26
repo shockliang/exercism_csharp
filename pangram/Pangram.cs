d166437ff15bd5289f5a8108c98679666a7b52b0
Shock Liang <shockwater@gmail.com>
2018-02-26 19:06:39 +0800

    Submit pangram solution version 1.

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        var ascillCodeOfLowerAlphabet = Enumerable.Range(97, 26);
+        var letters = input.ToLower().Where(x => char.IsLetter(x)).Select(x => Convert.ToInt32(x));
+        return ascillCodeOfLowerAlphabet.Except(letters).Count() == 0;
     }
 }
