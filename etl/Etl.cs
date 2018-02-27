59698e28dcbb3349df6f424e622a4b2805e1ad2d
Shock Liang <shockwater@gmail.com>
2018-02-28 00:01:45 +0800

    Submit etl solution version 1.

--- etl/Etl.cs
+++ etl/Etl.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class Etl
 {
     public static IDictionary<string, int> Transform(IDictionary<int, IList<string>> old)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return old.Aggregate(new Dictionary<string, int>(), (result, dic) =>
+        {
+            foreach (var item in dic.Value)
+            {
+                result.Add(item.ToLower(), dic.Key);
+            }
+            return result;
+        });
     }
 }
