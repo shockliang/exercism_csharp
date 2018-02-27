42fc0dc208c98251f43bf210abfac1b4fea1b844
Shock Liang <shockwater@gmail.com>
2018-02-28 00:02:49 +0800

    Submit etl solution version 2.

--- etl/Etl.cs
+++ etl/Etl.cs
@@ -6,13 +6,7 @@ public static class Etl
 {
     public static IDictionary<string, int> Transform(IDictionary<int, IList<string>> old)
     {
-        return old.Aggregate(new Dictionary<string, int>(), (result, dic) =>
-        {
-            foreach (var item in dic.Value)
-            {
-                result.Add(item.ToLower(), dic.Key);
-            }
-            return result;
-        });
+        return old.SelectMany(pair => pair.Value.Select(k => new { key = k.ToLower(), value = pair.Key }))
+          .ToDictionary(x => x.key, y => y.value);
     }
 }
