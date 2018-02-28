5a727c3ff9e629ba7ce0210180e5a0c5323d3ced
Shock Liang <shockwater@gmail.com>
2018-03-01 00:30:01 +0800

    Submit phone number solution version 2.

--- phone-number/PhoneNumber.cs
+++ phone-number/PhoneNumber.cs
@@ -6,28 +6,14 @@ public class PhoneNumber
 {
     public static string Clean(string phoneNumber)
     {
-        phoneNumber = new string(phoneNumber.Where(x => char.IsDigit(x)).ToArray());
-        if (!IsValidNumberLength(phoneNumber))
-            throw new ArgumentException();
+        var phone = new string(phoneNumber.Where(char.IsNumber).ToArray());
+        if (phone.First() == '1')
+        {
+            phone = phone.Substring(1);
+        }
 
-        phoneNumber = phoneNumber.Length == 10 ? phoneNumber.Insert(0, "1") : phoneNumber;
-        var digits = phoneNumber.Select(x => int.Parse(x.ToString())).ToArray();
-        var isValidCountryCode = digits[0] == 1;
-        var isValidAreaCode = IsValidRegualNumberRange(digits[1]);
-        var isValidExchangeCode = IsValidRegualNumberRange(digits[4]);
-
-        return isValidCountryCode && isValidAreaCode && isValidExchangeCode
-                    ? phoneNumber.TrimStart('1') : throw new ArgumentException();
+        return !IsValidPhoneNumber(phone) ? throw new ArgumentException() : phone;
     }
 
-    private static bool IsValidNumberLength(string phoneNumber)
-    {
-        return phoneNumber.Length > 9 && phoneNumber.Length < 12;
-    }
-
-    private static bool IsValidRegualNumberRange(int digit)
-    {
-        return digit > 1 && digit < 9;
-    }
-
+    private static bool IsValidPhoneNumber(string phoneNumber) => Regex.IsMatch(phoneNumber, @"^[2-9][0-9]{2}[2-9][0-9]{6}$");
 }
