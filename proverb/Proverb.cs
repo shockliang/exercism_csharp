c9dce3a3ad42a902b8c1abd17e4501a9a7b35082
Shock Liang <shockwater@gmail.com>
2018-03-04 00:25:52 +0800

    Submit proverb solution version 1.

--- proverb/Proverb.cs
+++ proverb/Proverb.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class Proverb
 {
+    private static readonly IEnumerable<string> words = new string[] { "nail", "shoe", "horse", "rider", "message", "battle", "kingdom" };
+    private static IList<string> proverbs => GenerateProverb(words);
+
+    private static IList<string> GenerateProverb(IEnumerable<string> words)
+    {
+        var _proverbs = new List<string>();
+        for (int i = 0; i < words.Count() - 1; i++)
+        {
+            var twoWords = words.Skip(i).Take(2).ToArray();
+            _proverbs.Add($"For want of a {twoWords[0]} the {twoWords[1]} was lost.");
+        }
+        _proverbs.Add("And all for the want of a horseshoe nail.");
+        return _proverbs;
+    }
+
     public static string Line(int line)
     {
-        throw new NotImplementedException();
+        return line > 0 && line <= proverbs.Count ? proverbs[line - 1] : "";
     }
 
     public static string AllLines()
     {
-        throw new NotImplementedException();
+        return string.Join("\n", proverbs);
     }
 }
