95b85d9b4d9d0a4062b182f8b35d3a64822d92a1
Shock Liang <shockwater@gmail.com>
2018-03-03 14:50:25 +0800

    Submit scrabble score solution version 1.

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class ScrabbleScore
 {
+    private static readonly IDictionary<string, int> scoreTable = new Dictionary<string, int>()
+    {
+        ["AEIOULNRST"] = 1,
+        ["DG"] = 2,
+        ["BCMP"] = 3,
+        ["FHVWY"] = 4,
+        ["K"] = 5,
+        ["JX"] = 8,
+        ["QZ"] = 10
+    };
+
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return input
+                .ToUpper()
+                .Select(letter => scoreTable.Where(x => x.Key.Contains(letter)).Select(x => x.Value).FirstOrDefault())
+                .Sum();
     }
+
+
 }
