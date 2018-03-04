df9adeae34bd7bc24a1182e3441d344f04796f31
Shock Liang <shockwater@gmail.com>
2018-03-04 17:08:55 +0800

    Submit robot name solution version 1.

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,17 +1,56 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Robot
 {
-    public string Name
+    private static IDictionary<string, List<int>> tags = new Dictionary<string, List<int>>();
+
+    private static IList<string> letterIds;
+    static Robot()
     {
-        get
+        var azLetters = Enumerable.Range(65, 26).Select(x => Convert.ToChar(x));
+        letterIds = azLetters.Aggregate(new List<string>(), (result, first) =>
         {
-            throw new NotImplementedException("You need to implement this function.");
+            result.AddRange(azLetters.Select(second => $"{first}{second}"));
+            return result;
+        });
+
+        foreach (var item in letterIds)
+        {
+            tags.Add(item, null);
         }
     }
 
+    private string letterId;
+    private int digitId;
+
+    public Robot()
+    {
+        Name = GetName();
+    }
+
+    public string Name { get; private set; }
+
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        tags[letterId].Add(digitId);
+        Name = GetName();
+    }
+
+    private string GetName()
+    {
+        var rand = new Random();
+        letterId = letterIds[rand.Next(0, letterIds.Count)];
+
+        if (tags[letterId] == null)
+            tags[letterId] = Enumerable.Range(0, 999).ToList();
+
+        var digitIds = tags[letterId];
+        var randDigitIndex = rand.Next(0, digitIds.Count);
+        digitId = digitIds[randDigitIndex];
+        digitIds.Remove(digitId);
+        
+        return $"{letterId}{digitId:D3}";
     }
 }
