5a85478fefd3745626d01d1ca30884e9cfc14d5c
Shock Liang <shockwater@gmail.com>
2018-03-05 20:46:11 +0800

    Submit protein translation solution version 1.

--- protein-translation/ProteinTranslation.cs
+++ protein-translation/ProteinTranslation.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class ProteinTranslation
 {
+    private static readonly IDictionary<string, string> translation = new Dictionary<string, string>()
+    {
+        ["AUG"] = "Methionine",
+        ["UUU"] = "Phenylalanine",
+        ["UUC"] = "Phenylalanine",
+        ["UUA"] = "Leucine",
+        ["UUG"] = "Leucine",
+        ["UCU"] = "Serine",
+        ["UCC"] = "Serine",
+        ["UCA"] = "Serine",
+        ["UCG"] = "Serine",
+        ["UAU"] = "Tyrosine",
+        ["UAC"] = "Tyrosine",
+        ["UGU"] = "Cysteine",
+        ["UGC"] = "Cysteine",
+        ["UGG"] = "Tryptophan",
+        ["UAA"] = "STOP",
+        ["UAG"] = "STOP",
+        ["UGA"] = "STOP",
+    };
     public static string[] Translate(string codon)
     {
-        throw new NotImplementedException();
+        codon = codon.ToUpper();
+        var codons = new List<string>();
+
+        for (int i = 0; i < codon.Length; i += 3)
+        {
+            var singleCodon = new string(codon.Skip(i).Take(3).ToArray());
+            if (translation.ContainsKey(singleCodon))
+            {
+                if(translation[singleCodon].Equals("STOP"))
+                    break;
+                else
+                    codons.Add(translation[singleCodon]);
+            }
+        }
+        return codons.Count > 0 ? codons.ToArray() : throw new Exception();
     }
 }
