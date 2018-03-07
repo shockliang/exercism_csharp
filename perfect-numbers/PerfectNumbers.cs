87126d0243d3790b7729aed37cab64d3ad7ae5fc
Shock Liang <shockwater@gmail.com>
2018-03-07 19:47:22 +0800

    Submit perfect number solution version 2 that inspried by @BlueD

--- perfect-numbers/PerfectNumbers.cs
+++ perfect-numbers/PerfectNumbers.cs
@@ -4,25 +4,13 @@ using System.Collections.Generic;
 
 public enum Classification
 {
-    Perfect,
-    Abundant,
-    Deficient
+    Perfect = 0,
+    Abundant = 1,
+    Deficient = -1
 }
 
 public static class PerfectNumbers
 {
-    public static Classification Classify(int number)
-    {
-        if (number <= 0)
-            throw new ArgumentOutOfRangeException();
-
-        var result = Enumerable.Range(1, number - 1).Sum(x => number % x == 0 ? x : 0);
-
-        if (result == number)
-            return Classification.Perfect;
-        else if (result > number)
-            return Classification.Abundant;
-        else
-            return Classification.Deficient;
-    }
+    public static Classification Classify(int number) =>
+        (Classification)Enumerable.Range(1, number - 1).Sum(x => number % x == 0 ? x : 0).CompareTo(number);
 }
