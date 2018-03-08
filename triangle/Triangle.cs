d3e04531b665d4b65ccb88e23e2802ee65c9f929
Shock Liang <shockwater@gmail.com>
2018-03-08 20:50:20 +0800

    Submit triangle solution version2.

--- triangle/Triangle.cs
+++ triangle/Triangle.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 // For a shape to be a triangle at all, all sides have to be of length > 0,
 // and the sum of the lengths of any two sides must be greater than or equal to the length of the third side.
@@ -7,36 +9,36 @@ public static class Triangle
     // A scalene triangle has all sides of different lengths
     public static bool IsScalene(double side1, double side2, double side3)
     {
-        return IsValidSides(side1, side2, side3) &&
-                side1 != side2 && side2 != side3 && side3 != side1;
+        var sides = new double[] { side1, side2, side3 };
+        return IsValidSides(sides) &&
+                sides.Distinct().Count() == 3;
     }
 
     // An isosceles triangle has at least two sides the same length.
     public static bool IsIsosceles(double side1, double side2, double side3)
     {
-        return IsValidSides(side1, side2, side3) &&
-                ((side1 == side2) ||
-                (side2 == side3) ||
-                (side3 == side1));
+        var sides = new double[] { side1, side2, side3 };
+        return IsValidSides(sides) &&
+                sides.Distinct().Count() == 2 ||
+                sides.Distinct().Count() == 1;
     }
 
     // An equilateral triangle has all three sides the same length
     public static bool IsEquilateral(double side1, double side2, double side3)
     {
-        return IsValidSides(side1, side2, side3) &&
-                (side1 == side2 && side2 == side3 && side1 == side3);
+        var sides = new double[] { side1, side2, side3 };
+        return IsValidSides(sides) &&
+                sides.Distinct().Count() == 1;
     }
 
-    private static bool IsAllGreatThanZero(double side1, double side2, double side3)
+    private static bool IsAllGreatThanZero(IEnumerable<double> sides)
     {
-        return side1 > 0 && side2 > 0 && side3 > 0;
+        return sides.Count() == 3 && sides.All(side => side > 0);
     }
-    private static bool IsValidSides(double side1, double side2, double side3)
+    private static bool IsValidSides(IEnumerable<double> sides)
     {
-        return IsAllGreatThanZero(side1, side2, side3) &&
-                (side1 + side2 >= side3 &&
-                side2 + side3 >= side1 &&
-                side3 + side1 >= side2);
+        return IsAllGreatThanZero(sides) &&
+                sides.Max() <= sides.Sum() / 2;
     }
 }
 
