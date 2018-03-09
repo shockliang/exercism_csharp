46778d769471e60c83d9ef5ad4a2c9960d4837e5
Shock Liang <shockwater@gmail.com>
2018-03-09 21:00:05 +0800

    Submit sieve solution version 1.

--- sieve/Sieve.cs
+++ sieve/Sieve.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class Sieve
 {
     public static int[] Primes(int limit)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return limit < 2 ? throw new ArgumentOutOfRangeException() :
+                Enumerable.Range(2, limit - 1)
+                .Aggregate(new List<int>(Enumerable.Range(2, limit - 1)), (primes, number) =>
+                {
+                    primes.RemoveAll(x => x % number == 0 && x != number);
+                    return primes;
+                })
+                .ToArray();
     }
 }
