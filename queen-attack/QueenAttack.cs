1c95e393890e1bbd390252c41c65c9899b5d4373
Shock Liang <shockwater@gmail.com>
2018-03-10 23:36:12 +0800

    Submit queen attack solution version 2.

--- queen-attack/QueenAttack.cs
+++ queen-attack/QueenAttack.cs
@@ -1,38 +1,29 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Numerics;
 
 public class Queen
 {
-    private static readonly int ChessboardSize = 8;
     public Queen(int row, int column)
     {
-        Row = row >= 0 && row < ChessboardSize ? row : throw new ArgumentOutOfRangeException();
-        Column = column >= 0 && column < ChessboardSize ? column : throw new ArgumentOutOfRangeException();
-        Vector = new Vector2(row, column);
+        Row = row;
+        Column = column;
     }
 
     public int Row { get; }
     public int Column { get; }
-    public Vector2 Vector { get; }
 }
 
 public static class QueenAttack
 {
-    private static readonly IEnumerable<double> diagonals = new double[] { 45.0d, -45.0d, 135.0d, -135.0d };
     public static bool CanAttack(Queen white, Queen black)
     {
-        var angle = Math.Atan2(black.Vector.Y - white.Vector.Y, black.Vector.X - white.Vector.X);
-        var radians = angle * (180.0 / Math.PI);
-
-        return white.Row == black.Row ||
-                white.Column == black.Column ||
-                diagonals.Any(x => x == radians);
+        int colDiff = white.Column - black.Column, rowDiff = white.Row - black.Row;
+        return colDiff == 0 || rowDiff == 0 || Math.Abs(colDiff) == Math.Abs(rowDiff);
     }
 
     public static Queen Create(int row, int column)
     {
+        if (Math.Min(row, column) < 0 || Math.Max(row, column) > 7) throw new ArgumentOutOfRangeException();
+
         return new Queen(row, column);
     }
 }
