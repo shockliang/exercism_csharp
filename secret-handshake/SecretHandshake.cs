da477816399d3fd658e006dc397d0d9c185f5044
Shock Liang <shockwater@gmail.com>
2018-03-12 17:25:09 +0800

    Submit secret handshake solution version 1.

--- secret-handshake/SecretHandshake.cs
+++ secret-handshake/SecretHandshake.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class SecretHandshake
 {
+    private static IDictionary<int, string> decodeMapping = new Dictionary<int, string>()
+    {
+        [0b01] = "wink",
+        [0b10] = "double blink",
+        [0b100] = "close your eyes",
+        [0b1000] = "jump",
+    };
     public static string[] Commands(int commandValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var decodes = new List<string>();
+
+        foreach (var item in decodeMapping)
+        {
+            if ((commandValue | item.Key) == commandValue)
+                decodes.Add(item.Value);
+        }
+
+        if ((commandValue | 0b10000) == commandValue)
+            decodes.Reverse();
+
+        return decodes.ToArray();
     }
 }
