dbcf64498f6bf2b0b45eae5caad98476b2a8e9bc
Shock Liang <shockwater@gmail.com>
2018-03-12 21:09:56 +0800

    Submit isgram solution version 3.

--- isogram/Isogram.cs
+++ isogram/Isogram.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Isogram
 {
+    private static readonly Regex findDup = new Regex(@"(.).*\1");
     public static bool IsIsogram(string word)
     {
-        return word
-                .Where(char.IsLetter)
-                .GroupBy(char.ToLower)
-                .All(g => g.Count() < 2);
+        return !findDup.IsMatch(new string(word.ToLower()
+                                               .Where(c => Char.IsLetter(c))
+                                               .ToArray()));
     }
 }
