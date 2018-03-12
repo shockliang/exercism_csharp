dec404ac89e32eb2ba8592e3390be8bb41810c81
Shock Liang <shockwater@gmail.com>
2018-03-12 21:00:56 +0800

    Submit isogram solution version 2.

--- isogram/Isogram.cs
+++ isogram/Isogram.cs
@@ -6,10 +6,9 @@ public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        word = word.ToLower();
         return word
-                .Where(x => !x.Equals(' ') && !x.Equals('-'))
-                .Where(c => word.Count(x => x == c) > 1)
-                .Count() == 0;
+                .Where(char.IsLetter)
+                .GroupBy(char.ToLower)
+                .All(g => g.Count() < 2);
     }
 }
