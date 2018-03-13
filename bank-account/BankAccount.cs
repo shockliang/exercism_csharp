2daa8408eba428b503977c3957455a8a1ada6af6
Shock Liang <shockwater@gmail.com>
2018-03-13 20:58:50 +0800

    Sumbit bank account solution iteration 2.

--- bank-account/BankAccount.cs
+++ bank-account/BankAccount.cs
@@ -1,36 +1,37 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 public class BankAccount
 {
-    private float currentBalance;
-    private bool isClose = false;
-    private object lockObject = new object();
+    private bool isClose = true;
+    private Decimal balance;
 
+    [MethodImpl(MethodImplOptions.Synchronized)]
     public void Open()
     {
-        lock (lockObject)
-        {
-            currentBalance = 0.0f;
-            isClose = false;
-        }
-
+        isClose = false;
+        balance = 0;
     }
 
+    [MethodImpl(MethodImplOptions.Synchronized)]
     public void Close() => isClose = true;
 
     public float Balance
     {
+        [MethodImpl(MethodImplOptions.Synchronized)]
         get
         {
-            return isClose ? throw new InvalidOperationException() : currentBalance;
+            return isClose ? throw new InvalidOperationException() : (float)balance;
         }
     }
 
+
+    [MethodImpl(MethodImplOptions.Synchronized)]
     public void UpdateBalance(float change)
     {
-        lock (lockObject)
-        {
-            currentBalance += change;
-        }
+        if (isClose)
+            throw new InvalidOperationException();
+
+        balance += (Decimal)change;
     }
 }
