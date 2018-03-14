2c6ebedcddde5fc1b15d981edf23d2e447dbf761
Shock Liang <shockwater@gmail.com>
2018-03-14 22:14:47 +0800

    Submit acronym solution iteration 2.

--- acronym/Acronym.cs
+++ acronym/Acronym.cs
@@ -4,16 +4,6 @@ using System.Collections.Generic;
 
 public static class Acronym
 {
-    public static string Abbreviate(string phrase)
-    {
-        var letters = phrase.Split(' ').Select(x => x.FirstOrDefault()).ToArray();
-
-        if (phrase.Contains(':'))
-            letters = phrase.Substring(0, phrase.IndexOf(':')).ToArray();
-
-        if (phrase.Contains('-'))
-            letters = phrase.Split(new char[] { ' ', '-' }).Select(x => x.FirstOrDefault()).ToArray();
-
-        return new string(letters).ToUpper();
-    }
+    public static string Abbreviate(string phrase) 
+        => new string(phrase.Split(' ', '-').Select(s => char.ToUpper(s[0])).ToArray());
 }
