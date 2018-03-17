43ba26fb434d9da8b4ef560f93f4430a52cd4eb1
Shock Liang <shockwater@gmail.com>
2018-03-17 18:04:56 +0800

    Submit rotational cipher solution iteration 1.

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = text
+            .Select((c) =>
+            {
+                if (char.IsLetter(c))
+                {
+                    var converted = Convert.ToChar((c + (shiftKey % 26)));
+                    if ((char.IsLower(c) && converted > 'z') ||
+                        (char.IsUpper(c) && converted > 'Z'))
+                        return Convert.ToChar(converted - 26);
+                    else
+                        return converted;
+                }
+                else
+                {
+                    return c;
+                }
+            })
+            .ToArray();
+        return new string(result);
     }
 }
