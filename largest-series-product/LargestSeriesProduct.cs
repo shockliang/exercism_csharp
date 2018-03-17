735fbf2e0d0699e5d08a02f8d229486f07277fb0
Shock Liang <shockwater@gmail.com>
2018-03-18 00:40:27 +0800

    Submit largest series product solution iteration 2.

--- largest-series-product/LargestSeriesProduct.cs
+++ largest-series-product/LargestSeriesProduct.cs
@@ -1,39 +1,16 @@
 using System;
 using System.Linq;
-using System.Collections.Generic;
 
 public static class LargestSeriesProduct
 {
     public static long GetLargestProduct(string digits, int span)
     {
-        if (IsInvalidValue(digits, span))
+        if (span > digits.Length || span < 0 || digits.Any(c => c > '9' || c < '0'))
             throw new ArgumentException();
-        if (IsReportToOne(digits, span))
-            return 1L;
 
-        List<long> products = new List<long>();
-        var count = digits.Length % span == 0 ? digits.Length : digits.Length - span + 1;
-        for (int i = 0; i < count; i++)
-        {
-            var product = digits
-                            .Skip(i).Take(span)
-                            .Select(x => long.Parse(char.ToString(x)))
-                            .Aggregate((result, digit) => result * digit);
-            products.Add(product);
-        }
-        return products.Max();
-    }
-
-    private static bool IsInvalidValue(string digits, int span)
-    {
-        return digits.Length < span ||
-                span < 0 ||
-                digits.Where(char.IsDigit).Count() != digits.Length;
-    }
-
-    private static bool IsReportToOne(string digits, int span)
-    {
-        return string.IsNullOrEmpty(digits) && span == 0 ||
-                digits.Length > 0 && span == 0;
+        var ints = digits.Select(c => c - '0');
+        return Enumerable.Range(0, digits.Length - span + 1)
+                .Select(i => ints.Skip(i).Take(span))
+                .Max(s => s.Aggregate(1, (i, p) => i * p));
     }
 }
