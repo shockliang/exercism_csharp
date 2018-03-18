e91688c378549293b536be570a7f03266b44f800
Shock Liang <shockwater@gmail.com>
2018-03-18 20:52:35 +0800

    Initial house project.

---
+++ house/House.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class House
+{
+    public static string Recite(int startVerse, int endVerse)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
