7fc608f983516dad94489b6f0cbcb9f52437637d
Shock Liang <shockwater@gmail.com>
2018-03-19 22:44:01 +0800

    Submit pythagorean triplet solution iteration 1.

--- pythagorean-triplet/PythagoreanTriplet.cs
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -1,29 +1,49 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class Triplet
 {
+    private readonly int a;
+    private readonly int b;
+    private readonly int c;
+
+    private readonly IEnumerable<int> sides;
+
     public Triplet(int a, int b, int c)
     {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        sides = new int[] { a, b, c };
     }
 
-    public int Sum()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public int Sum() => sides.Sum();
 
-    public int Product()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public int Product() => sides.Aggregate((result, side) => result * side);
 
-    public bool IsPythagorean()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public bool IsPythagorean() => a * a + b * b == c * c;
 
     public static IEnumerable<Triplet> Where(int maxFactor, int minFactor = 1, int sum = 0)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var triplets = new List<Triplet>();
+        for (int i = minFactor; i <= maxFactor; i++)
+        {
+            for (int j = i; j <= maxFactor; j++)
+            {
+                for (int k = j; k <= maxFactor; k++)
+                {
+                    if (i * i + j * j == k * k)
+                    {
+                        var triplet = new Triplet(i, j, k);
+                        if (triplet.Sum() == sum)
+                            triplets.Add(triplet);
+                        else if (sum == 0)
+                            triplets.Add(triplet);
+                    }
+                }
+            }
+        }
+        return triplets;
     }
 }
