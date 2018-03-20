cf7071fdbd9a515bb13ef6457b347dfad12c7c43
Shock Liang <shockwater@gmail.com>
2018-03-21 00:10:14 +0800

    Submit twelve days solution iteration 1.

--- twelve-days/TwelveDays.cs
+++ twelve-days/TwelveDays.cs
@@ -1,19 +1,75 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 public static class TwelveDaysSong
 {
-    public static string Sing()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static string Sing() => string.Join("", Verses(1, 12));
 
     public static string Verse(int verseNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var verses = new StringBuilder($"On the {ConvertDigitToOrdinal(verseNumber)} day of Christmas my true love gave to me, ");
+
+        for (int i = verseNumber; i >= 1; i--)
+        {
+            var symbol = i == 1 ? ".\n" : ", ";
+            var gift = verseNumber > 1 && i == 1
+                ? $"and {DayOfGift(i)}{symbol}"
+                : $"{DayOfGift(i)}{symbol}";
+
+            verses.Append(gift);
+        }
+
+        return verses.ToString();
     }
 
     public static string Verses(int start, int end)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return string.Join("", Enumerable.Range(start, end - start + 1)
+                                .Select(i => Verse(i))
+                                .Select(verse => $"{verse}\n"));
+    }
+
+    private static string ConvertDigitToOrdinal(int i)
+    {
+        switch (i)
+        {
+            case 1: return "first";
+            case 2: return "second";
+            case 3: return "third";
+            case 4: return "fourth";
+            case 5: return "fifth";
+            case 6: return "sixth";
+            case 7: return "seventh";
+            case 8: return "eighth";
+            case 9: return "ninth";
+            case 10: return "tenth";
+            case 11: return "eleventh";
+            case 12: return "twelfth";
+            default:
+                throw new IndexOutOfRangeException();
+        }
+    }
+
+    private static string DayOfGift(int i)
+    {
+        switch (i)
+        {
+            case 1: return "a Partridge in a Pear Tree";
+            case 2: return "two Turtle Doves";
+            case 3: return "three French Hens";
+            case 4: return "four Calling Birds";
+            case 5: return "five Gold Rings";
+            case 6: return "six Geese-a-Laying";
+            case 7: return "seven Swans-a-Swimming";
+            case 8: return "eight Maids-a-Milking";
+            case 9: return "nine Ladies Dancing";
+            case 10: return "ten Lords-a-Leaping";
+            case 11: return "eleven Pipers Piping";
+            case 12: return "twelve Drummers Drumming";
+            default:
+                throw new IndexOutOfRangeException();
+        }
     }
 }
--- twelve-days/TwelveDaysTest.cs
+++ twelve-days/TwelveDaysTest.cs
@@ -10,7 +10,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(1));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_2()
     {
         var expected = "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -18,7 +18,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(2));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_3()
     {
         var expected = "On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -26,7 +26,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(3));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_4()
     {
         var expected = "On the fourth day of Christmas my true love gave to me, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -34,7 +34,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(4));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_5()
     {
         var expected = "On the fifth day of Christmas my true love gave to me, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -42,7 +42,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(5));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_6()
     {
         var expected = "On the sixth day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -50,7 +50,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(6));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_7()
     {
         var expected = "On the seventh day of Christmas my true love gave to me, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -58,7 +58,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(7));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_8()
     {
         var expected = "On the eighth day of Christmas my true love gave to me, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -66,7 +66,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(8));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_9()
     {
         var expected = "On the ninth day of Christmas my true love gave to me, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -74,7 +74,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(9));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_10()
     {
         var expected = "On the tenth day of Christmas my true love gave to me, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -82,7 +82,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(10));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_11()
     {
         var expected = "On the eleventh day of Christmas my true love gave to me, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -90,7 +90,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(11));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_verse_12()
     {
         var expected = "On the twelfth day of Christmas my true love gave to me, twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
@@ -98,7 +98,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verse(12));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_multiple_verses()
     {
         var expected = "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n\n" +
@@ -108,7 +108,7 @@ public class TwelveDaysTest
         Assert.Equal(expected, TwelveDaysSong.Verses(1, 3));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Return_entire_song()
     {
         Assert.Equal(TwelveDaysSong.Sing(), TwelveDaysSong.Verses(1, 12));
