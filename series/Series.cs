7c362b61c997ba9f3cec4de45c5acc3b44700733
Shock Liang <shockwater@gmail.com>
2018-03-20 22:55:53 +0800

    Submit series solution iteration 2.

--- series/Series.cs
+++ series/Series.cs
@@ -4,19 +4,18 @@ using System.Collections.Generic;
 
 public class Series
 {
-    private readonly string numbers;
+    private readonly IEnumerable<int> numbers;
 
     public Series(string numbers)
     {
-        this.numbers = numbers;
+        this.numbers = numbers.Select(c => (int)char.GetNumericValue(c));
     }
 
     public int[][] Slices(int sliceLength)
     {
-        return sliceLength > numbers.Length
+        return sliceLength > numbers.Count()
             ? throw new ArgumentException()
-            : Enumerable.Range(0, numbers.Length - sliceLength + 1)
-                .Select(i => numbers.Skip(i).Take(sliceLength)
-                .Select(c => (int)char.GetNumericValue(c)).ToArray()).ToArray();
+            : Enumerable.Range(0, numbers.Count() - sliceLength + 1)
+                .Select(i => numbers.Skip(i).Take(sliceLength).ToArray()).ToArray();
     }
 }
