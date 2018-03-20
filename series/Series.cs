0d1eaba56e9de62c935442f057255c02dc5fdff5
Shock Liang <shockwater@gmail.com>
2018-03-20 22:48:51 +0800

    Submit series solution iteration 1.

--- series/Series.cs
+++ series/Series.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Series
 {
+    private readonly string numbers;
+
     public Series(string numbers)
     {
+        this.numbers = numbers;
     }
 
     public int[][] Slices(int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return sliceLength > numbers.Length
+            ? throw new ArgumentException()
+            : Enumerable.Range(0, numbers.Length - sliceLength + 1)
+                .Select(i => numbers.Skip(i).Take(sliceLength)
+                .Select(c => (int)char.GetNumericValue(c)).ToArray()).ToArray();
     }
 }
