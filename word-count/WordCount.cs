635b739b399f81662164bd971a5988d3df46dd5c
Shock Liang <shockwater@gmail.com>
2018-03-23 21:35:34 +0800

    Submit word count solution iteration 2.

--- word-count/WordCount.cs
+++ word-count/WordCount.cs
@@ -1,27 +1,14 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class WordCount
 {
-    public static IDictionary<string, int> Countwords(string phrase)
-    {
-        return phrase
-                .ToLower()
-                .Split(' ', '\n', ',', ':', '.')
-                .Where(w => !string.IsNullOrEmpty(w))
-                .Aggregate(new Dictionary<string, int>(), (dic, word) =>
-                {
-                    if (word.StartsWith('\'') && word.EndsWith('\''))
-                        word = new string(word.Where(char.IsLetterOrDigit).ToArray());
-                    else if (!word.Contains('\''))
-                        word = new string(word.Where(char.IsLetterOrDigit).ToArray());
+    private static char[] Separators = " ,\n\":.!@$%^&".ToCharArray();
 
-                    if (dic.ContainsKey(word))
-                        dic[word]++;
-                    else
-                        dic.Add(word, 1);
-                    return dic;
-                });
-    }
+    public static IDictionary<string, int> Countwords(string phrase) =>
+        phrase
+            .Split(Separators, StringSplitOptions.RemoveEmptyEntries)
+            .GroupBy(w => w.Trim('\'').ToLowerInvariant())
+            .ToDictionary(group => group.Key, group => group.Count());
 }
