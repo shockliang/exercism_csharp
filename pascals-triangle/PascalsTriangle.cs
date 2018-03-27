1718ea555eac0a0157eff2a1aa0111daf11629ed
Shock Liang <shockwater@gmail.com>
2018-03-27 20:50:32 +0800

    Submit pascals triangle solution iteration 1.

--- pascals-triangle/PascalsTriangle.cs
+++ pascals-triangle/PascalsTriangle.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class PascalsTriangle
 {
     public static IEnumerable<IEnumerable<int>> Calculate(int rows)
     {
-        throw new NotImplementedException();
+        if (rows < 0) throw new ArgumentOutOfRangeException();
+
+        var result = new List<List<int>>();
+        int c = 1;
+        for (int i = 0; i < rows; i++)
+        {
+            var row = new List<int>();
+            for (int j = 0; j <= i; j++)
+            {
+                c = j == 0 || i == 0 ? 1 : c * (i - j + 1) / j;
+
+                row.Add(c);
+            }
+            result.Add(row);
+        }
+
+        return result;
     }
 
 }
