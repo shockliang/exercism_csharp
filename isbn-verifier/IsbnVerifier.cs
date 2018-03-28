7156f0d3c86f50915a88127454e53c9d9a36e3b4
Shock Liang <shockwater@gmail.com>
2018-03-28 21:48:21 +0800

    Submit isbn verifier solution iteration 1.

--- isbn-verifier/IsbnVerifier.cs
+++ isbn-verifier/IsbnVerifier.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class IsbnVerifier
 {
     public static bool IsValid(string number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        number = number.ToLower();
+
+        if (Invalid(number))
+            return false;
+
+        var digits = number
+            .Where(char.IsLetterOrDigit)
+            .Select(d =>
+            {
+                var digit = 0;
+                if (char.IsDigit(d))
+                    digit = (int)char.GetNumericValue(d);
+                else if (d == 'x')
+                    digit = 10;
+                return digit;
+            });
+
+        var digitQueue = new Queue<int>(digits);
+        var checkSum = 0;
+        for (int i = digitQueue.Count; i > 0; i--)
+        {
+            checkSum += (digitQueue.Dequeue() * i);
+        }
+
+        return checkSum % 11 == 0;
+    }
+
+    private static bool Invalid(string number)
+    {
+        return number.Where(char.IsLetterOrDigit).Count() != 10 ||
+                (char.IsLetter(number.Last()) && !number.EndsWith('x')) ||
+                (number.Contains('x') && number.IndexOf('x') != number.Length - 1);
     }
 }
