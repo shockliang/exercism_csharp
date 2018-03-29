07fa1d9f5fd58296b20cb2669a805c50b167dc90
Shock Liang <shockwater@gmail.com>
2018-03-29 22:02:44 +0800

    Submit list ops solution iteration 2.

--- list-ops/ListOps.cs
+++ list-ops/ListOps.cs
@@ -5,46 +5,16 @@ using System.Linq;
 public static class ListOps
 {
     public static int Length<T>(List<T> input)
-    {
-        var count = 0;
-        foreach (var item in input)
-        {
-            count++;
-        }
-        return count;
-    }
+        => Foldl((count, item) => count + 1, 0, input);
 
     public static List<T> Reverse<T>(List<T> input)
-    {
-        var reversed = new List<T>();
-        for (int i = Length(input) - 1; i >= 0; i--)
-        {
-            reversed.Add(input[i]);
-        }
-        return reversed;
-    }
+        => Foldl((list, item) => Append(new List<T> { item }, list), new List<T>(), input);
 
     public static List<TOut> Map<TIn, TOut>(Func<TIn, TOut> map, List<TIn> input)
-    {
-        var mapList = new List<TOut>();
-        foreach (var item in input)
-        {
-            mapList.Add(map.Invoke(item));
-        }
-
-        return mapList;
-    }
+        => Foldl((list, item) => Append(list, new List<TOut>() { map.Invoke(item) }), new List<TOut>(), input);
 
     public static List<T> Filter<T>(Func<T, bool> predicate, List<T> input)
-    {
-        var filtered = new List<T>();
-        foreach (var item in input)
-        {
-            if (predicate.Invoke(item))
-                filtered.Add(item);
-        }
-        return filtered;
-    }
+        => Foldl((list, item) => predicate.Invoke(item) ? Append(list, new List<T>() { item }) : list, new List<T>(), input);
 
     public static TOut Foldl<TIn, TOut>(Func<TOut, TIn, TOut> func, TOut start, List<TIn> input)
     {
@@ -59,38 +29,11 @@ public static class ListOps
     }
 
     public static TOut Foldr<TIn, TOut>(Func<TIn, TOut, TOut> func, TOut start, List<TIn> input)
-    {
-        TOut result = start;
-        var reversed = Reverse(input);
-        foreach (var item in reversed)
-        {
-            result = func.Invoke(item, result);
-        }
-
-        return result;
-    }
+        => Foldl((list, item) => func.Invoke(item, list), start, Reverse(input));
 
     public static List<T> Concat<T>(List<List<T>> input)
-    {
-        var result = new List<T>();
-
-        foreach (var item in input)
-        {
-            result = Append(result, item);
-        }
-
-        return result;
-    }
+        => Foldl((list, item) => Append(list, item), new List<T>(), input);
 
     public static List<T> Append<T>(List<T> left, List<T> right)
-    {
-        var result = new List<T>(left);
-
-        foreach (var item in right)
-        {
-            result.Add(item);
-        }
-
-        return result;
-    }
+        => Foldl((list, item) => { list.Add(item); return list; }, new List<T>(left), right);
 }
