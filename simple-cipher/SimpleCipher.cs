f5ad9f738de633b9b96fa9ede1e7fa3370f669e8
Shock Liang <shockwater@gmail.com>
2018-03-30 23:57:45 +0800

    Submit simple cipher solution iteration 2.

--- simple-cipher/SimpleCipher.cs
+++ simple-cipher/SimpleCipher.cs
@@ -1,53 +1,54 @@
 using System;
 using System.Linq;
-using System.Collections.Generic;
+using System.Text;
 using static System.String;
 
 public class Cipher
 {
     public Cipher()
+        : this(GenerateRandomKey())
     {
-        Key = GenerateRandomKey();
     }
 
-    public Cipher(string key)
-    {
-        Key = IsNullOrEmpty(key) ||
-                key.Where(char.IsUpper).Count() > 0 ||
-                key.Where(char.IsDigit).Count() > 0
-            ? throw new ArgumentException()
-            : key;
-    }
-
-    public string Key { get; }
-
-    public string Encode(string plaintext)
-    {
-        return Concat(plaintext
-                .Select((c, idx) => Rollover(c + (Key[idx] - 'a'))));
-    }
-
-    public string Decode(string ciphertext)
-    {
-        return Concat(ciphertext
-                .Select((c, idx) =>
-                {
-                    var shift = (Key[idx] - 'a');
-                    var isRollover = (c - shift) < 'a';
-                    return isRollover ? (char)('z' - (shift - (c - 'a')) + 1) : (char)(c - shift);
-                }));
-    }
-
-    private char Rollover(int letter)
-    {
-        return letter > 'z' ? (char)('a' + letter - 'z' - 1) : (char)letter;
-    }
-
-    private string GenerateRandomKey()
+    private static string GenerateRandomKey()
     {
         var rand = new Random();
         return Concat(Enumerable.Range(0, 100)
                 .Select(i => rand.Next('a', 'z'))
                 .Select(x => Convert.ToChar(x)));
+    }
+
+    public Cipher(string key)
+    {
+        Key = key.Any() && key.All(c => c >= 'a' && c <= 'z') ? key : throw new ArgumentException();
+    }
+    
+    public string Key { get; private set; }
+
+    public string Encode(string plaintext)
+    {
+        return Shift(plaintext, Key, 1);
+    }
+
+    public string Decode(string ciphertext)
+    {
+        return Shift(ciphertext, Key, -1);
+    }
+
+    private static string Shift(string input, string key, int sign)
+    {
+        var shiftedChars = input
+            .Zip(key, (c,k) => Shift(c, k, sign))
+            .ToArray();
+        return new string(shiftedChars);
+    }
+
+    private static char Shift(char input, char key, int sign)
+    {
+        int charAsInt = input - 'a';
+        int shiftBy = (key - 'a') * sign;
+        int shiftedValue = charAsInt + shiftBy + 26;
+        int indexInAlphabet = shiftedValue % 26;
+        return (char)('a' + indexInAlphabet);
     }
 }
