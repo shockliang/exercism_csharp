8646ff2a8a8e60be3f0bf6b3366eee358715d6a5
Shock Liang <shockwater@gmail.com>
2018-03-30 23:42:27 +0800

    Submit simple cipher solution iteration 1.

--- simple-cipher/SimpleCipher.cs
+++ simple-cipher/SimpleCipher.cs
@@ -1,32 +1,53 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using static System.String;
 
 public class Cipher
 {
     public Cipher()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Key = GenerateRandomKey();
     }
 
     public Cipher(string key)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Key = IsNullOrEmpty(key) ||
+                key.Where(char.IsUpper).Count() > 0 ||
+                key.Where(char.IsDigit).Count() > 0
+            ? throw new ArgumentException()
+            : key;
     }
-    
-    public string Key 
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+
+    public string Key { get; }
 
     public string Encode(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Concat(plaintext
+                .Select((c, idx) => Rollover(c + (Key[idx] - 'a'))));
     }
 
     public string Decode(string ciphertext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Concat(ciphertext
+                .Select((c, idx) =>
+                {
+                    var shift = (Key[idx] - 'a');
+                    var isRollover = (c - shift) < 'a';
+                    return isRollover ? (char)('z' - (shift - (c - 'a')) + 1) : (char)(c - shift);
+                }));
+    }
+
+    private char Rollover(int letter)
+    {
+        return letter > 'z' ? (char)('a' + letter - 'z' - 1) : (char)letter;
+    }
+
+    private string GenerateRandomKey()
+    {
+        var rand = new Random();
+        return Concat(Enumerable.Range(0, 100)
+                .Select(i => rand.Next('a', 'z'))
+                .Select(x => Convert.ToChar(x)));
     }
 }
