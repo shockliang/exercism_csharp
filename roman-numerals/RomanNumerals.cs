678353b35af0936f196776123bb56fa9463b99d2
Shock Liang <shockwater@gmail.com>
2018-03-31 23:45:29 +0800

    Submit roman numerals solution iteration 2.

--- roman-numerals/RomanNumerals.cs
+++ roman-numerals/RomanNumerals.cs
@@ -4,23 +4,33 @@ using System.Collections.Generic;
 
 public static class RomanNumeralExtension
 {
+    public static string ToRoman(this int n, char one, char five, char ten)
+    {
+        switch (n % 10)
+        {
+            case 1: return new string(new char[] { one });
+            case 2: return new string(new char[] { one, one });
+            case 3: return new string(new char[] { one, one, one });
+            case 4: return new string(new char[] { one, five });
+            case 5: return new string(new char[] { five });
+            case 6: return new string(new char[] { five, one });
+            case 7: return new string(new char[] { five, one, one });
+            case 8: return new string(new char[] { five, one, one, one });
+            case 9: return new string(new char[] { one, ten});
+        }
+        return "";
+    }
+
     public static string ToRoman(this int value)
     {
-        if ((value < 0) || (value > 3999)) throw new ArgumentOutOfRangeException();
-        if (value < 1) return string.Empty;
-        if (value >= 1000) return "M" + (value - 1000).ToRoman();
-        if (value >= 900) return "CM" + (value - 900).ToRoman();
-        if (value >= 500) return "D" + (value - 500).ToRoman();
-        if (value >= 400) return "CD" + (value - 400).ToRoman();
-        if (value >= 100) return "C" + (value - 100).ToRoman();
-        if (value >= 90) return "XC" + (value - 90).ToRoman();
-        if (value >= 50) return "L" + (value - 50).ToRoman();
-        if (value >= 40) return "XL" + (value - 40).ToRoman();
-        if (value >= 10) return "X" + (value - 10).ToRoman();
-        if (value >= 9) return "IX" + (value - 9).ToRoman();
-        if (value >= 5) return "V" + (value - 5).ToRoman();
-        if (value >= 4) return "IV" + (value - 4).ToRoman();
-        if (value >= 1) return "I" + (value - 1).ToRoman();
-        throw new ArgumentOutOfRangeException();
+        List<string> numerals = new List<string>()
+        {
+            (value / 1000).ToRoman('M', ' ', ' '),
+            (value / 100).ToRoman('C', 'D', 'M'),
+            (value / 10).ToRoman('X', 'L', 'C'),
+            (value / 1).ToRoman('I', 'V', 'X'),
+        };
+
+        return string.Join("", numerals);
     }
 }
