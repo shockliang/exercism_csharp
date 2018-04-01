f0653cd599fadf5bee55c3a81a1c1a99168621ac
Shock Liang <shockwater@gmail.com>
2018-04-01 13:20:08 +0800

    Submit flatten array solution iteration 2.

--- flatten-array/FlattenArray.cs
+++ flatten-array/FlattenArray.cs
@@ -1,33 +1,20 @@
 ﻿using System;
-using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class FlattenArray
 {
     public static IEnumerable Flatten(IEnumerable input)
     {
-        var list = new List<object>();
-
         foreach (var item in input)
         {
-            switch (item)
-            {
-                case IEnumerable nest:
-                    foreach (var nestItem in nest)
-                    {
-                        if (nestItem is IEnumerable)
-                            list.AddRange(Flatten(nestItem as IEnumerable) as IEnumerable<object>);
-                        else if (nestItem != null)
-                            list.Add(nestItem);
-                    }
-                    break;
-
-                case object obj:
-                    list.Add(obj);
-                    break;
-            }
+            if (item == null) continue;
+            if (item is IEnumerable nest)
+                foreach (var nestItem in Flatten(nest))
+                    yield return nestItem;
+            else
+                yield return item;
         }
-        return list;
     }
 }
