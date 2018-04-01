6b6bf6327805479ea02ea9e49c71ba6223224bb6
Shock Liang <shockwater@gmail.com>
2018-04-01 16:31:34 +0800

    Submit binary search solution iteration 1.

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -1,14 +1,39 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public class BinarySearch
 {
+    private readonly int[] input;
     public BinarySearch(int[] input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        this.input = input;
     }
 
     public int Find(int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var count = 0;
+        var mid = input.Length % 2 == 0 ? (input.Length / 2) + 1 : input.Length / 2;
+        var previous = input.Length;
+        while (true && input.Length > 0)
+        {
+            var midElement = input[mid];
+            if (midElement == value)
+            {
+                return mid;
+            }
+            else if (midElement > value)
+            {
+                previous = mid;
+                mid /= 2;
+            }
+            else
+            {
+                mid = ((previous - mid) / 2) + mid;
+            }
+
+            if (count++ == input.Length) { break; }
+        }
+        return -1;
     }
 }
