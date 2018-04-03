839415a385c14bd902964808c1c0f4a01f2bb62c
Shock Liang <shockwater@gmail.com>
2018-04-03 21:29:28 +0800

    Submit parallel letter frequency solution iteration 1.

--- parallel-letter-frequency/ParallelLetterFrequency.cs
+++ parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public static class ParallelLetterFrequency
 {
     public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
     {
-        throw new NotImplementedException();
+        var letters = new ConcurrentDictionary<char, int>();
+
+        Parallel.ForEach(texts, (text) =>
+        {
+            lock (text)
+            {
+                var letterGroup = text.ToLower().Where(char.IsLetter).GroupBy(c => c);
+                foreach (var item in letterGroup)
+                {
+                    if (letters.ContainsKey(item.Key))
+                        letters[item.Key] += item.Count();
+                    else
+                        letters.TryAdd(item.Key, item.Count());
+                }
+            }
+        });
+
+        return letters.ToDictionary(kvp => kvp.Key,
+                                    kvp => kvp.Value);
     }
 }
