2f6f2bca75f0552821aee94248690ab030afaa29
Shock Liang <shockwater@gmail.com>
2018-04-04 18:41:09 +0800

    Submit atbash cipher solution iteration 1.

--- atbash-cipher/AtbashCipher.cs
+++ atbash-cipher/AtbashCipher.cs
@@ -1,14 +1,49 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using static System.String;
 
 public static class AtbashCipher
 {
+    private static IDictionary<char, char> mapping;
+
+    static AtbashCipher()
+    {
+        mapping = new Dictionary<char, char>();
+        var az = Enumerable.Range('a', 26).ToArray();
+        for (int i = 0; i < az.Count(); i++)
+        {
+            mapping.Add((char)az[i], (char)az[az.Count() - i - 1]);
+        }
+    }
+
     public static string Encode(string plainValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var filtered = plainValue
+            .ToLower()
+            .Where(char.IsLetterOrDigit)
+            .Select(c => mapping.ContainsKey(c) ? mapping[c] : c)
+            .ToList();
+
+        var fiveLetters = new List<string>();
+        for (int i = 0; i < filtered.Count(); i += 5)
+        {
+            fiveLetters.Add(Concat(filtered.Skip(i).Take(5)));
+        }
+        return Join(' ', fiveLetters);
     }
 
     public static string Decode(string encodedValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = new List<char>();
+        var filtered = encodedValue.Where(char.IsLetterOrDigit);
+
+        foreach (var letter in filtered)
+        {
+            result.Add(char.IsLetter(letter) 
+                ? mapping.FirstOrDefault(x => x.Value.Equals(letter)).Key 
+                : letter);
+        }
+        return Concat(result);
     }
 }
