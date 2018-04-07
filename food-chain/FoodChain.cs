cc907738feb511b887efd7a36a2ae0b525480a42
Shock Liang <shockwater@gmail.com>
2018-04-07 16:15:56 +0800

    Initial food chain project.

---
+++ food-chain/FoodChain.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class FoodChain
+{
+    public static string Recite(int startVerse, int endVerse)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
