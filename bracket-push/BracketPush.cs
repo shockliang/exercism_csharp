50630ea148bcee36a4af7e8bfa477ac446942694
Shock Liang <shockwater@gmail.com>
2018-04-09 23:38:06 +0800

    Submit bracket push solution iteration 1.

--- bracket-push/BracketPush.cs
+++ bracket-push/BracketPush.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class BracketPush
 {
     public static bool IsPaired(string input)
     {
-        throw new NotImplementedException();
+        var filtered = input
+            .Where(c => c.Equals('(') || c.Equals(')') ||
+                        c.Equals('[') || c.Equals(']') ||
+                        c.Equals('{') || c.Equals('}'))
+            .ToArray();
+
+        var brackets = string.Concat(filtered);
+
+        while (brackets.Length > 0)
+        {
+            brackets = brackets.Replace("()", "").Replace("[]", "").Replace("{}", "");
+            
+            if (brackets.Length == filtered.Length || brackets.Length % 2 != 0)
+            {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
--- bracket-push/BracketPushTest.cs
+++ bracket-push/BracketPushTest.cs
@@ -11,91 +11,91 @@ public class BracketPushTest
         Assert.True(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Empty_string()
     {
         var input = "";
         Assert.True(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Unpaired_brackets()
     {
         var input = "[[";
         Assert.False(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Wrong_ordered_brackets()
     {
         var input = "}{";
         Assert.False(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Wrong_closing_bracket()
     {
         var input = "{]";
         Assert.False(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Paired_with_whitespace()
     {
         var input = "{ }";
         Assert.True(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Simple_nested_brackets()
     {
         var input = "{[]}";
         Assert.True(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Several_paired_brackets()
     {
         var input = "{}[]";
         Assert.True(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Paired_and_nested_brackets()
     {
         var input = "([{}({}[])])";
         Assert.True(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Unopened_closing_brackets()
     {
         var input = "{[)][]}";
         Assert.False(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Unpaired_and_nested_brackets()
     {
         var input = "([{])";
         Assert.False(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Paired_and_wrong_nested_brackets()
     {
         var input = "[({]})";
         Assert.False(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Math_expression()
     {
         var input = "(((185 + 223.85) * 15) - 543)/2";
         Assert.True(BracketPush.IsPaired(input));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Complex_latex_expression()
     {
         var input = "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)";
