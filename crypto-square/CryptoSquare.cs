6fe6a463f5759c2cfcc9a75d6e38c2f26e8d19fd
Shock Liang <shockwater@gmail.com>
2018-04-10 21:31:38 +0800

    Submit crypto square solution iteration 1.

--- crypto-square/CryptoSquare.cs
+++ crypto-square/CryptoSquare.cs
@@ -1,25 +1,53 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using static System.String;
 
 public static class CryptoSquare
 {
-    public static string NormalizedPlaintext(string plaintext)
+    public static string NormalizedPlaintext(this string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Concat(plaintext.ToLower().Where(char.IsLetterOrDigit));
     }
 
-    public static IEnumerable<string> PlaintextSegments(string plaintext)
+    public static IEnumerable<string> PlaintextSegments(this string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var col = (int)Math.Ceiling(Math.Sqrt(plaintext.Length));
+
+        var list = new List<string>();
+        for (int i = 0; i < plaintext.Length; i += col)
+        {
+            list.Add(Concat(plaintext.Skip(i).Take(col)));
+        }
+
+        return list;
     }
 
-    public static string Encoded(string plaintext)
+    public static string Encoded(this string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var segments = plaintext.Split(" ");
+        var col = segments.First().Count();
+        var list = new List<string>();
+
+        for (int i = 0; i < segments.Length; i++)
+        {
+            if (segments[i].Length < col)
+                segments[i] = segments[i].PadRight(col);
+        }
+
+        for (int i = 0; i < col; i++)
+        {
+            list.Add(Concat(segments.Select(s => s.ElementAt(i))));
+        }
+
+        return Join(" ", list);
     }
 
     public static string Ciphertext(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Join(" ", plaintext
+            .NormalizedPlaintext()
+            .PlaintextSegments())
+            .Encoded();
     }
 }
