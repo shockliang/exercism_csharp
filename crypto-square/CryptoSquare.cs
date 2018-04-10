187d50f092a2f5ef2bfc879b8e4756c772f6d864
Shock Liang <shockwater@gmail.com>
2018-04-10 22:02:12 +0800

    Submt crypto square solution iteration 2.

--- crypto-square/CryptoSquare.cs
+++ crypto-square/CryptoSquare.cs
@@ -7,40 +7,30 @@ public static class CryptoSquare
 {
     public static string NormalizedPlaintext(this string plaintext)
     {
-        return Concat(plaintext.ToLower().Where(char.IsLetterOrDigit));
+        return Concat(plaintext
+            .ToLower()
+            .Where(char.IsLetterOrDigit));
     }
 
     public static IEnumerable<string> PlaintextSegments(this string plaintext)
     {
         var col = (int)Math.Ceiling(Math.Sqrt(plaintext.Length));
 
-        var list = new List<string>();
-        for (int i = 0; i < plaintext.Length; i += col)
-        {
-            list.Add(Concat(plaintext.Skip(i).Take(col)));
-        }
-
-        return list;
+        return Enumerable.Range(0, plaintext.Length)
+            .Where(i => i % col == 0)
+            .Select(i => Concat(plaintext.Skip(i).Take(col)));
     }
 
     public static string Encoded(this string plaintext)
     {
         var segments = plaintext.Split(" ");
         var col = segments.First().Count();
-        var list = new List<string>();
 
-        for (int i = 0; i < segments.Length; i++)
-        {
-            if (segments[i].Length < col)
-                segments[i] = segments[i].PadRight(col);
-        }
+        var paddingSegments = Enumerable.Range(0, segments.Length)
+            .Select(i => segments[i].PadRight(col));
 
-        for (int i = 0; i < col; i++)
-        {
-            list.Add(Concat(segments.Select(s => s.ElementAt(i))));
-        }
-
-        return Join(" ", list);
+        return Join(" ", Enumerable.Range(0, col)
+            .Select(i => Concat(paddingSegments.Select(s => s.ElementAt(i)))));
     }
 
     public static string Ciphertext(string plaintext)
