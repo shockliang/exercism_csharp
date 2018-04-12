5031705ae4a3ac8f3e65bc77103791ada2ff7d7b
Shock Liang <shockwater@gmail.com>
2018-04-12 23:24:29 +0800

    Submit custom set solution iteration 1.

--- custom-set/CustomSet.cs
+++ custom-set/CustomSet.cs
@@ -1,88 +1,123 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
 public class CustomSet<T> : IEnumerable<T>, IEquatable<IEnumerable<T>>
 {
+    private IEnumerable<T> values;
+
     public CustomSet()
     {
+        this.values = new T[] { };
     }
 
     public CustomSet(T value)
     {
+        this.values = value == null ? new T[] { } : new T[] { value };
     }
 
     public CustomSet(IEnumerable<T> values)
     {
+        this.values = values.OrderBy(x => x);
     }
 
     public CustomSet<T> Insert(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!Contains(value))
+            values = values.Append(value).OrderBy(x => x);
+        return this;
     }
 
     public bool IsEmpty()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return values.Equals(null) || values.Count().Equals(0);
     }
 
     public bool Contains(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return values.Contains(value);
     }
 
     public bool IsSubsetOf(CustomSet<T> right)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (this.IsEmpty() && right.IsEmpty())
+            return true;
+
+        if (this.IsEmpty() && !right.IsEmpty())
+            return true;
+
+        if (this.Equals(right))
+            return true;
+
+        if (right.Count() > this.Count())
+            return values.SequenceEqual(right.Intersect(values));
+
+        return false;
     }
 
     public bool IsDisjointFrom(CustomSet<T> right)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (this.IsEmpty() && right.IsEmpty())
+            return true;
+
+        if (this.IsEmpty() && !right.IsEmpty())
+            return true;
+
+        if (!this.IsEmpty() && right.IsEmpty())
+            return true;
+
+        return Equals(this.Except(right));
     }
 
     public CustomSet<T> Intersection(CustomSet<T> right)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new CustomSet<T>(this.Intersect(right).OrderBy(x => x));
     }
 
     public CustomSet<T> Difference(CustomSet<T> right)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var reuslt = this.Except(right);
+        return new CustomSet<T>(this.Except(right).OrderBy(x => x));
     }
 
     public CustomSet<T> Union(CustomSet<T> right)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = values.Union(right).OrderByDescending(x => x);
+        return new CustomSet<T>(result);
     }
 
     public IEnumerator<T> GetEnumerator()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (var item in values)
+            yield return item;
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-    
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
     public override bool Equals(object obj)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (obj.Equals(null))
+            return false;
+
+        if (obj is IEnumerable<T>)
+            return Equals(obj as IEnumerable<T>);
+        else
+            return false;
     }
 
-    public override int GetHashCode() 
+    public override int GetHashCode()
     {
-        throw new NotImplementedException();
+        return values.GetHashCode();
     }
 
     public bool Equals(IEnumerable<T> other)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return values.SequenceEqual(other);
     }
 
     public int GetHashCode(IEnumerable<T> obj)
     {
-        throw new NotImplementedException();
+        return obj.GetHashCode();
     }
 }
