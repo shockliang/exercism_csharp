90b6a2c8d324cb53969554c044864680fdaacb56
Shock Liang <shockwater@gmail.com>
2018-04-12 23:38:55 +0800

    Submit custom set solution iteration 2.

--- custom-set/CustomSet.cs
+++ custom-set/CustomSet.cs
@@ -7,20 +7,13 @@ public class CustomSet<T> : IEnumerable<T>, IEquatable<IEnumerable<T>>
 {
     private IEnumerable<T> values;
 
-    public CustomSet()
-    {
-        this.values = new T[] { };
-    }
+    public CustomSet() => this.values = new T[] { };
 
     public CustomSet(T value)
-    {
-        this.values = value == null ? new T[] { } : new T[] { value };
-    }
+        => this.values = value == null ? new T[] { } : new T[] { value };
 
     public CustomSet(IEnumerable<T> values)
-    {
-        this.values = values.OrderBy(x => x);
-    }
+        => this.values = values.OrderBy(x => x);
 
     public CustomSet<T> Insert(T value)
     {
@@ -29,63 +22,22 @@ public class CustomSet<T> : IEnumerable<T>, IEquatable<IEnumerable<T>>
         return this;
     }
 
-    public bool IsEmpty()
-    {
-        return values.Equals(null) || values.Count().Equals(0);
-    }
+    public bool IsEmpty() => values.Equals(null) || values.Count().Equals(0);
 
-    public bool Contains(T value)
-    {
-        return values.Contains(value);
-    }
+    public bool Contains(T value) => values.Contains(value);
 
-    public bool IsSubsetOf(CustomSet<T> right)
-    {
-        if (this.IsEmpty() && right.IsEmpty())
-            return true;
+    public bool IsSubsetOf(CustomSet<T> right) => Equals(right.Intersection(this));
 
-        if (this.IsEmpty() && !right.IsEmpty())
-            return true;
-
-        if (this.Equals(right))
-            return true;
-
-        if (right.Count() > this.Count())
-            return values.SequenceEqual(right.Intersect(values));
-
-        return false;
-    }
-
-    public bool IsDisjointFrom(CustomSet<T> right)
-    {
-        if (this.IsEmpty() && right.IsEmpty())
-            return true;
-
-        if (this.IsEmpty() && !right.IsEmpty())
-            return true;
-
-        if (!this.IsEmpty() && right.IsEmpty())
-            return true;
-
-        return Equals(this.Except(right));
-    }
+    public bool IsDisjointFrom(CustomSet<T> right) => Equals(this.Difference(right));
 
     public CustomSet<T> Intersection(CustomSet<T> right)
-    {
-        return new CustomSet<T>(this.Intersect(right).OrderBy(x => x));
-    }
+        => new CustomSet<T>(this.Intersect(right).OrderBy(x => x));
 
     public CustomSet<T> Difference(CustomSet<T> right)
-    {
-        var reuslt = this.Except(right);
-        return new CustomSet<T>(this.Except(right).OrderBy(x => x));
-    }
+        => new CustomSet<T>(this.Except(right).OrderBy(x => x));
 
     public CustomSet<T> Union(CustomSet<T> right)
-    {
-        var result = values.Union(right).OrderByDescending(x => x);
-        return new CustomSet<T>(result);
-    }
+        => new CustomSet<T>(values.Union(right).OrderByDescending(x => x));
 
     public IEnumerator<T> GetEnumerator()
     {
@@ -106,18 +58,9 @@ public class CustomSet<T> : IEnumerable<T>, IEquatable<IEnumerable<T>>
             return false;
     }
 
-    public override int GetHashCode()
-    {
-        return values.GetHashCode();
-    }
+    public override int GetHashCode() => values.GetHashCode();
 
-    public bool Equals(IEnumerable<T> other)
-    {
-        return values.SequenceEqual(other);
-    }
+    public bool Equals(IEnumerable<T> other) => values.SequenceEqual(other);
 
-    public int GetHashCode(IEnumerable<T> obj)
-    {
-        return obj.GetHashCode();
-    }
+    public int GetHashCode(IEnumerable<T> obj) => obj.GetHashCode();
 }
