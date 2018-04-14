c5afd8449c5c5b8b84d5ec38787d1886a9323a4f
Shock Liang <shockwater@gmail.com>
2018-04-15 00:39:32 +0800

    Submit ocr numbers solution iteration 2.

--- ocr-numbers/OcrNumbers.cs
+++ ocr-numbers/OcrNumbers.cs
@@ -20,35 +20,27 @@ public static class OcrNumbers
 
     public static string Convert(string input)
     {
-        var result = input.Split("\n").ToArray();
-        var nums = new List<string>();
-        var rows = new List<string[]>();
-
-        for (int i = 0; i < result.Length; i += 4)
-        {
-            rows.Add(result.Skip(i).Take(4).ToArray());
-        }
-
-        foreach (var row in rows)
-        {
-            if (row.Length < 4 || row.First().Count() % 3 != 0)
-                throw new ArgumentException();
-                
-            var singleRow = new List<string>();
-            for (int i = 0; i < row.First().Count(); i += 3)
+        return string.Join(",", Enumerable
+            .Range(0, input.Split("\n").Length)
+            .Where(i => i % 4 == 0)
+            .Select(i => input.Split("\n").Skip(i).Take(4).ToArray())
+            .Select(row =>
             {
-                var key = string.Concat(
-                    row[0].Skip(i).Take(3).Concat(
-                    row[1].Skip(i).Take(3)).Concat(
-                    row[2].Skip(i).Take(3)));
-                if (patterns.ContainsKey(key))
-                    singleRow.Add(patterns[key]);
-                else
-                    singleRow.Add("?");
-            }
-            nums.Add(string.Concat(singleRow));
-        }
+                if (row.Length < 4 || row.First().Count() % 3 != 0)
+                    throw new ArgumentException();
 
-        return string.Join(",", nums);
+                var singleRow = new List<string>();
+                for (int i = 0; i < row.First().Count(); i += 3)
+                {
+                    var key = string.Concat(
+                        row[0].Skip(i).Take(3).Concat(
+                        row[1].Skip(i).Take(3)).Concat(
+                        row[2].Skip(i).Take(3)));
+
+                    singleRow.Add(patterns.ContainsKey(key) ? patterns[key] : "?");
+                }
+
+                return string.Concat(singleRow);
+            }));
     }
 }
