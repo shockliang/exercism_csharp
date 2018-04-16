c207b75ff2bfc6c3226e08c18a0ce3a00c165a56
Shock Liang <shockwater@gmail.com>
2018-04-16 22:50:24 +0800

    Submit luhn solution iteration 1.

--- luhn/Luhn.cs
+++ luhn/Luhn.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class Luhn
 {
     public static bool IsValid(string number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (number.Where(char.IsDigit).Count() <= 1 ||
+            number.Where(c => c != ' ' && c < '0' || c > '9').Count() > 0)
+            return false;
+
+        return number
+            .Where(char.IsDigit)
+            .Select(c => c - '0')
+            .Reverse()
+            .Select((digit, i) =>
+            {
+                if ((i + 1) % 2 == 0)
+                    return digit * 2 > 9 ? digit * 2 - 9 : digit * 2;
+                else
+                    return digit;
+            })
+            .Sum() % 10 == 0;
     }
 }
