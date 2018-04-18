0e14f5dec15d298f325992ad37f5db049ab5b4b7
Shock Liang <shockwater@gmail.com>
2018-04-19 01:12:24 +0800

    Submit spiral matrix solution iteration 1.

--- spiral-matrix/SpiralMatrix.cs
+++ spiral-matrix/SpiralMatrix.cs
@@ -1,9 +1,200 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public class SpiralMatrix
 {
+    [Flags]
+    public enum Direction : int
+    {
+        Right = 0b1000,
+        Bottom = 0b0100,
+        Left = 0b0010,
+        Top = 0b0001,
+        All = Right | Bottom | Left | Top,
+    }
+
     public static int[,] GetMatrix(int size)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var cells = new Cell<int>[size, size];
+        var matrix = new int[size, size];
+
+        for (int c = 0; c < size; c++)
+        {
+            for (int r = 0; r < size; r++)
+            {
+                var cell = new Cell<int>(0, r, c);
+                cells[r, c] = cell;
+                cell.CheckBound(size);
+            }
+        }
+
+        var directions = new Direction[] {
+                Direction.Right, Direction.Bottom, Direction.Left,Direction.Top}.ToList();
+
+        for (int c = 0; c < size; c++)
+        {
+            for (int r = 0; r < size; r++)
+            {
+                var cell = cells[r, c];
+                directions.ForEach(dir =>
+                {
+                    if ((cell.Spaces & dir) == dir)
+                    {
+                        switch (dir)
+                        {
+                            case Direction.Right:
+                                cell.SetNeighbor(Direction.Right, cells[r, c + 1]);
+                                break;
+                            case Direction.Bottom:
+                                cell.SetNeighbor(Direction.Bottom, cells[r + 1, c]);
+                                break;
+                            case Direction.Left:
+                                cell.SetNeighbor(Direction.Left, cells[r, c - 1]);
+                                break;
+                            case Direction.Top:
+                                cell.SetNeighbor(Direction.Top, cells[r - 1, c]);
+                                break;
+                        }
+                    }
+                });
+
+            }
+        }
+
+        int step = 1, row = 0, col = 0;
+        var nextDirection = Direction.Right;
+        while (step <= (size * size))
+        {
+            var cell = cells[row, col];
+            cell.Value = step;
+            cell.UpdateSpaces();
+            nextDirection = cell.NextDirection;
+
+            switch (nextDirection)
+            {
+                case Direction.Right:
+                    col++;
+                    break;
+                case Direction.Bottom:
+                    row++;
+                    break;
+                case Direction.Left:
+                    col--;
+                    break;
+                case Direction.Top:
+                    row--;
+                    break;
+            }
+
+            step++;
+        }
+        var values = OutputValues(cells);
+        return OutputValues(cells);
+    }
+
+    private static T[,] OutputValues<T>(Cell<T>[,] cells)
+    {
+        var values = new T[cells.GetLength(0), cells.GetLength(1)];
+        var size = cells.GetLength(0);
+
+        for (int c = 0; c < size; c++)
+        {
+            for (int r = 0; r < size; r++)
+            {
+                values[r, c] = cells[r, c].Value;
+            }
+        }
+        return values;
+    }
+
+    public class Cell<T>
+    {
+        public T Value { get; set; }
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+        public bool HasSpaceOnTheLeft { get; set; } = true;
+        public bool HasSpaceOnTheBottom { get; set; } = true;
+        public bool HasSpaceOnTheRight { get; set; } = true;
+        public bool HasSpaceOnTheTop { get; set; } = true;
+        public Direction Spaces { get; set; } = Direction.All;
+        public Direction NextDirection { get; private set; } = Direction.Left;
+        private IDictionary<Direction, Cell<T>> neighbors = new Dictionary<Direction, Cell<T>>(4);
+
+        public Cell(T value, int row, int col)
+        {
+            Value = value;
+            Row = row;
+            Col = col;
+        }
+
+        // neighbor
+        public void CheckBound(int bound)
+        {
+            if (Col == 0)
+            {
+                HasSpaceOnTheLeft = false;
+                Spaces ^= Direction.Left;
+            }
+            if (Row == 0)
+            {
+                HasSpaceOnTheTop = false;
+                Spaces ^= Direction.Top;
+            }
+            if (Col + 1 == bound)
+            {
+                HasSpaceOnTheRight = false;
+                Spaces ^= Direction.Right;
+            }
+            if (Row + 1 == bound)
+            {
+                HasSpaceOnTheBottom = false;
+                Spaces ^= Direction.Bottom;
+            }
+
+            NextDirection = GetNextDirection();
+        }
+
+        public void UpdateSpaces()
+        {
+            foreach (var neighbor in neighbors)
+            {
+                if (!neighbor.Value.Value.Equals(default(T)))
+                {
+                    Spaces ^= neighbor.Key;
+                }
+            }
+
+            var directions = new Direction[] {
+                Direction.Right, Direction.Bottom, Direction.Left,Direction.Top};
+
+            var currentSpaces = directions.Where(dir => (Spaces & dir) == dir);
+
+            if (currentSpaces.Contains(Direction.Top) &&
+                currentSpaces.Contains(Direction.Right))
+            {
+                NextDirection = GetNextDirection(new Direction[] { Direction.Top, Direction.Right });
+            }
+            else
+            {
+                NextDirection = GetNextDirection();
+            }
+        }
+
+        public Direction GetNextDirection(IEnumerable<Direction> directionPriority = null)
+        {
+            directionPriority = directionPriority ?? new Direction[] {
+                Direction.Right, Direction.Bottom, Direction.Left,Direction.Top};
+
+            return directionPriority.FirstOrDefault(dir => (dir & Spaces) != 0);
+        }
+
+        public void SetNeighbor(Direction direction, Cell<T> neighbor)
+        {
+            if (neighbors.ContainsKey(direction))
+                neighbors[direction] = neighbor;
+            else
+                neighbors.Add(direction, neighbor);
+        }
     }
 }
--- spiral-matrix/SpiralMatrixTest.cs
+++ spiral-matrix/SpiralMatrixTest.cs
@@ -11,68 +11,101 @@ public class SpiralMatrixTest
         Assert.Equal(spiral, SpiralMatrix.GetMatrix(0));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Trivial_spiral()
     {
-        var spiral = new int[,] 
-        { 
+        var spiral = new int[,]
+        {
             { 1 }
         };
 
         Assert.Equal(spiral, SpiralMatrix.GetMatrix(1));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Spiral_of_size_2()
     {
-        var spiral = new int[,] 
-        { 
-            { 1, 2 }, 
+        var spiral = new int[,]
+        {
+            { 1, 2 },
             { 4, 3 }
         };
 
         Assert.Equal(spiral, SpiralMatrix.GetMatrix(2));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Spiral_of_size_3()
     {
-        var spiral = new int[,] 
-        { 
-            { 1, 2, 3 }, 
-            { 8, 9, 4 }, 
+        var spiral = new int[,]
+        {
+            { 1, 2, 3 },
+            { 8, 9, 4 },
             { 7, 6, 5 }
         };
 
         Assert.Equal(spiral, SpiralMatrix.GetMatrix(3));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Spiral_of_size_4()
     {
-        var spiral = new int[,] 
-        { 
-            { 1, 2, 3, 4 }, 
-            { 12, 13, 14, 5 }, 
-            { 11, 16, 15, 6 }, 
+        var spiral = new int[,]
+        {
+            { 1, 2, 3, 4 },
+            { 12, 13, 14, 5 },
+            { 11, 16, 15, 6 },
             { 10, 9, 8, 7}
         };
 
         Assert.Equal(spiral, SpiralMatrix.GetMatrix(4));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Spiral_of_size_5()
     {
-        var spiral = new int[,] 
-        { 
-            { 1, 2, 3, 4, 5 }, 
-            { 16, 17, 18, 19, 6 }, 
-            { 15, 24, 25, 20, 7 }, 
-            { 14, 23, 22, 21, 8 }, 
+        var spiral = new int[,]
+        {
+            { 1, 2, 3, 4, 5 },
+            { 16, 17, 18, 19, 6 },
+            { 15, 24, 25, 20, 7 },
+            { 14, 23, 22, 21, 8 },
             { 13, 12, 11, 10, 9 }
         };
 
         Assert.Equal(spiral, SpiralMatrix.GetMatrix(5));
+    }
+
+    [Fact]
+    public void Spiral_of_size_6()
+    {
+        var spiral = new int[,]
+        {
+            { 1, 2, 3, 4, 5, 6},
+            { 20, 21, 22, 23, 24, 7},
+            { 19, 32, 33, 34, 25, 8},
+            { 18, 31, 36, 35, 26, 9},
+            { 17, 30, 29, 28, 27, 10},
+            { 16, 15, 14, 13, 12, 11},
+        };
+
+        Assert.Equal(spiral, SpiralMatrix.GetMatrix(6));
+    }
+
+    [Fact]
+    public void Spiral_of_size_7()
+    {
+        var spiral = new int[,]
+        {
+            { 1, 2, 3, 4, 5, 6, 7},
+            { 24, 25, 26, 27, 28, 29, 8},
+            { 23, 40, 41, 42, 43, 30, 9},
+            { 22, 39, 48, 49, 44, 31, 10},
+            { 21, 38, 47, 46, 45, 32, 11},
+            { 20, 37, 36, 35, 34, 33, 12},
+            { 19, 18, 17, 16, 15, 14, 13},
+        };
+
+        Assert.Equal(spiral, SpiralMatrix.GetMatrix(7));
     }
 }
