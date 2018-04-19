ba0d965a4844d45d3b82190c448d13311277bf97
Shock Liang <shockwater@gmail.com>
2018-04-20 00:35:02 +0800

    Submit spiral matrix solution iteration 2.

--- spiral-matrix/SpiralMatrix.cs
+++ spiral-matrix/SpiralMatrix.cs
@@ -1,179 +1,49 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 
 public class SpiralMatrix
 {
-    [Flags]
-    public enum Direction : int
-    {
-        Right = 0b1000,
-        Bottom = 0b0100,
-        Left = 0b0010,
-        Top = 0b0001,
-        All = Right | Bottom | Left | Top,
-    }
-
     public static int[,] GetMatrix(int size)
     {
-        var cells = new Cell<int>[size, size];
-        var matrix = new int[size, size];
+        if (size < 1)
+            return new int[,] { };
 
-        for (int c = 0; c < size; c++)
+        var map = new int[size, size];
+        var row = 0;
+        var col = 0;
+        var dir = ConsoleKey.RightArrow;
+        var moves = 0;
+        var total = size * size;
+        map[row, col] = ++moves;
+        while (moves < total)
         {
-            for (int r = 0; r < size; r++)
+            switch (dir)
             {
-                var cell = new Cell<int>(0, r, c);
-                cells[r, c] = cell;
-                cell.CheckBound(size);
-            }
-        }
-
-        var directions = new Direction[] {
-                Direction.Right, Direction.Bottom, Direction.Left,Direction.Top}.ToList();
-
-        for (int c = 0; c < size; c++)
-        {
-            for (int r = 0; r < size; r++)
-            {
-                var cell = cells[r, c];
-                directions.ForEach(dir =>
-                {
-                    if ((cell.Spaces & dir) == dir)
-                    {
-                        switch (dir)
-                        {
-                            case Direction.Right:
-                                cell.SetNeighbor(Direction.Right, cells[r, c + 1]);
-                                break;
-                            case Direction.Bottom:
-                                cell.SetNeighbor(Direction.Bottom, cells[r + 1, c]);
-                                break;
-                            case Direction.Left:
-                                cell.SetNeighbor(Direction.Left, cells[r, c - 1]);
-                                break;
-                            case Direction.Top:
-                                cell.SetNeighbor(Direction.Top, cells[r - 1, c]);
-                                break;
-                        }
-                    }
-                });
-
-            }
-        }
-
-        int step = 1, row = 0, col = 0;
-        var nextDirection = Direction.Right;
-        while (step <= (size * size))
-        {
-            var cell = cells[row, col];
-            cell.Value = step;
-            cell.UpdateSpaces();
-            nextDirection = cell.NextDirection;
-
-            switch (nextDirection)
-            {
-                case Direction.Right:
-                    col++;
+                case ConsoleKey.RightArrow:
+                    if (col + 1 < size && map[row, col + 1] == 0)
+                        map[row, ++col] = ++moves;
+                    else
+                        dir = ConsoleKey.DownArrow;
                     break;
-                case Direction.Bottom:
-                    row++;
+                case ConsoleKey.DownArrow:
+                    if (row + 1 < size && map[row + 1, col] == 0)
+                        map[++row, col] = ++moves;
+                    else
+                        dir = ConsoleKey.LeftArrow;
                     break;
-                case Direction.Left:
-                    col--;
+                case ConsoleKey.LeftArrow:
+                    if (col - 1 >= 0 && map[row, col - 1] == 0)
+                        map[row, --col] = ++moves;
+                    else
+                        dir = ConsoleKey.UpArrow;
                     break;
-                case Direction.Top:
-                    row--;
+                case ConsoleKey.UpArrow:
+                    if (row - 1 >= 0 && map[row - 1, col] == 0)
+                        map[--row, col] = ++moves;
+                    else
+                        dir = ConsoleKey.RightArrow;
                     break;
             }
-
-            step++;
         }
-        
-        return OutputValues(cells);
-    }
-
-    private static T[,] OutputValues<T>(Cell<T>[,] cells)
-    {
-        var values = new T[cells.GetLength(0), cells.GetLength(1)];
-        var size = cells.GetLength(0);
-
-        for (int c = 0; c < size; c++)
-        {
-            for (int r = 0; r < size; r++)
-            {
-                values[r, c] = cells[r, c].Value;
-            }
-        }
-        return values;
-    }
-
-    public class Cell<T>
-    {
-        public T Value { get; set; }
-        public int Row { get; private set; }
-        public int Col { get; private set; }
-        public Direction Spaces { get; set; } = Direction.All;
-        public Direction NextDirection { get; private set; } = Direction.Left;
-        private IDictionary<Direction, Cell<T>> neighbors = new Dictionary<Direction, Cell<T>>(4);
-
-        public Cell(T value, int row, int col)
-        {
-            Value = value;
-            Row = row;
-            Col = col;
-        }
-
-        public void CheckBound(int bound)
-        {
-            if (Col == 0) { Spaces ^= Direction.Left; }
-            if (Row == 0) { Spaces ^= Direction.Top; }
-            if (Col + 1 == bound) { Spaces ^= Direction.Right; }
-            if (Row + 1 == bound) { Spaces ^= Direction.Bottom; }
-
-            NextDirection = GetNextDirection();
-        }
-
-        public void UpdateSpaces()
-        {
-            foreach (var neighbor in neighbors)
-            {
-                if (!neighbor.Value.Value.Equals(default(T)))
-                {
-                    Spaces ^= neighbor.Key;
-                }
-            }
-
-            var directions = new Direction[] {
-                Direction.Right, Direction.Bottom, Direction.Left,Direction.Top};
-
-            var currentSpaces = directions.Where(dir => (Spaces & dir) == dir);
-
-            if (currentSpaces.Contains(Direction.Top) &&
-                currentSpaces.Contains(Direction.Right))
-            {
-                NextDirection = GetNextDirection(new Direction[] { Direction.Top, Direction.Right });
-            }
-            else
-            {
-                NextDirection = GetNextDirection();
-            }
-        }
-
-        public Direction GetNextDirection(IEnumerable<Direction> directionPriority = null)
-        {
-            directionPriority = directionPriority ?? new Direction[] {
-                Direction.Right, Direction.Bottom, Direction.Left,Direction.Top};
-
-            return directionPriority.FirstOrDefault(dir => (dir & Spaces) != 0);
-        }
-
-        public void SetNeighbor(Direction direction, Cell<T> neighbor)
-        {
-            if (neighbors.ContainsKey(direction))
-                neighbors[direction] = neighbor;
-            else
-                neighbors.Add(direction, neighbor);
-        }
+        return map;
     }
 }
