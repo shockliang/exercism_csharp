0736db5268b912a933139d6eead5794bb4a8326c
shocklaing <shockwater@gmail.com>
2018-05-01 17:59:45 +0800

    Submit rail fence cipher solution iteration 2.

--- rail-fence-cipher/RailFenceCipher.cs
+++ rail-fence-cipher/RailFenceCipher.cs
@@ -14,26 +14,12 @@ public class RailFenceCipher
     {
         var encoded = BuildMatrix();
 
-        var dir = 1;
+        var increment = 1;
 
         for (int row = 0, col = 0; col < input.Length; col++)
         {
             encoded[row].Add(input[col]);
-
-            if (row == rails - 1)
-            {
-                dir = -1;
-                row = rails - 2;
-            }
-            else if (row == 0 && col != 0)
-            {
-                dir = 1;
-                row = 1;
-            }
-            else
-            {
-                row += dir;
-            }
+            (row, increment) = NextBoundary(row, increment);
         }
 
         return string.Concat(encoded.Select(c => string.Concat(c)));
@@ -42,55 +28,41 @@ public class RailFenceCipher
     public string Decode(string input)
     {
         var decoded = BuildMatrix(input.Length);
-        var dir = 1;
+        var increment = 1;
         var idx = 0;
         for (int currentRow = 0; currentRow < decoded.Count; currentRow++)
         {
-            dir = 1;
+            increment = 1;
             for (int row = 0, col = 0; col < input.Length; col++)
             {
                 if (row == currentRow)
                     decoded[row][col] = input[idx++];
-
-                if (row == rails - 1)
-                {
-                    dir = -1;
-                    row = rails - 2;
-                }
-                else if (row == 0 && col != 0)
-                {
-                    dir = 1;
-                    row = 1;
-                }
-                else
-                {
-                    row += dir;
-                }
+                (row, increment) = NextBoundary(row, increment);
             }
         }
 
         var decodedString = new List<char>();
-        dir = 1;
+        increment = 1;
         for (int row = 0, col = 0; col < input.Length; col++)
         {
             decodedString.Add(decoded[row][col]);
-            if (row == rails - 1)
-            {
-                dir = -1;
-                row = rails - 2;
-            }
-            else if (row == 0 && col != 0)
-            {
-                dir = 1;
-                row = 1;
-            }
-            else
-            {
-                row += dir;
-            }
+            (row, increment) = NextBoundary(row, increment);
         }
 
         return string.Concat(decodedString);
+    }
+
+    private (int row, int increment) NextBoundary(int currentRow, int currentIncrement)
+    {
+        var row = currentRow;
+        var increment = currentIncrement;
+
+        if (row + increment == rails || row + increment == -1)
+            increment *= -1;
+
+        row += increment;
+
+        return (row: row, increment: increment);
     }
 
     private List<List<char>> BuildMatrix(int length = 0)
