b71f2020bc93c0b6090f125e52ad567312083a7a
shocklaing <shockwater@gmail.com>
2018-05-05 23:13:19 +0800

    Submit change solution iteration 1.

--- change/Change.cs
+++ change/Change.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class Change
 {
     public static int[] Calculate(int target, int[] coins)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (coins.Any(coin => coin == target))
+            return new int[] { target };
+        if (target == 0)
+            return new int[0];
+        if (coins.All(coin => coin > target && target > 0) || target < 0)
+            throw new ArgumentException();
+
+        var combinations = new List<List<int>>();
+        var filtered = coins.Where(coin => coin < target).Reverse();
+
+        for (int i = 0; i < filtered.Count(); i++)
+        {
+            var list = filtered.Skip(i).Take(filtered.Count() - i).ToList();
+            var changes = new List<int>();
+            var remainder = target;
+            var j = 0;
+            while (remainder > 0)
+            {
+                if (remainder >= list[j])
+                {
+                    remainder -= list[j];
+                    changes.Add(list[j]);
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            changes.Reverse();
+            combinations.Add(changes);
+        }
+
+        return combinations.OrderBy(x => x.Count).FirstOrDefault().ToArray();
     }
 }
