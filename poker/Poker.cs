24dd3fdb44047308d47d3adc9a4ca7ad4beac070
shocklaing <shockwater@gmail.com>
2018-05-08 23:15:43 +0800

    Implementing the straight flush & straight & flush validators and test cases.

--- poker/Poker.cs
+++ poker/Poker.cs
@@ -1,10 +1,150 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public static class Poker
 {
     public static IEnumerable<string> BestHands(IEnumerable<string> hands)
     {
-        throw new NotImplementedException();
+        if (hands.Count() == 1)
+            return hands;
+
+        var pokerHands = hands.Select(hand => new Hand(hand));
+
+        return new string[] { };
+    }
+}
+
+// public enum Suit : int
+// {
+//     Spade,
+//     Hearts,
+//     Diamond,
+//     Club
+// }
+
+public enum Ranking : int
+{
+    StraightFlush = 0,
+    FourOfAKind,
+    FullHouse,
+    Flush,
+    Straight,
+    ThreeOfAKind,
+    TwoPair,
+    OnePair,
+    HighCard,
+}
+
+public class Hand
+{
+    public Ranking Ranking { get; private set; }
+    public IEnumerable<Card> Cards { get; private set; }
+    public Hand(string hand)
+    {
+        Cards = hand
+            .Split(' ')
+            .Select(card => new Card(card));
+    }
+}
+
+public class Card
+{
+    public char Suit { get; private set; }
+    public char Symbol { get; private set; }
+    public Card(string card)
+    {
+        Symbol = card.FirstOrDefault();
+        Suit = card.LastOrDefault();
+    }
+}
+
+public class RankingValidator
+{
+    private Dictionary<Ranking, Func<Hand, bool>> validators;
+    public RankingValidator()
+    {
+        validators = new Dictionary<Ranking, Func<Hand, bool>>()
+        {
+            [Ranking.StraightFlush] = IsStraightFlush,
+            [Ranking.FourOfAKind] = IsFourOfAKind,
+            [Ranking.FullHouse] = IsFullHouse,
+            [Ranking.Flush] = IsFlush,
+            [Ranking.Straight] = IsStraight,
+            [Ranking.ThreeOfAKind] = IsThreeOfAKind,
+            [Ranking.TwoPair] = IsTwoPair,
+            [Ranking.OnePair] = IsOnePair,
+            [Ranking.HighCard] = IsHighCard,
+        };
+    }
+
+    public bool IsStraightFlush(Hand hand)
+    {
+        return IsFlush(hand) && IsStraight(hand);
+    }
+
+    public bool IsFourOfAKind(Hand hand)
+    {
+        return true;
+    }
+
+    public bool IsFullHouse(Hand hand)
+    {
+        return true;
+    }
+
+    public bool IsFlush(Hand hand)
+    {
+        return hand.Cards.All(card => card.Suit == hand.Cards.FirstOrDefault().Suit);
+    }
+
+    public bool IsStraight(Hand hand)
+    {
+        var symbols = hand.Cards.Select(card => card.SymbolId()).OrderBy(x => x);
+        var start = symbols.FirstOrDefault();
+        var sequence = Enumerable.Range(start, 5);
+
+        return symbols.SequenceEqual(sequence);
+    }
+
+    public bool IsThreeOfAKind(Hand hand)
+    {
+        return true;
+    }
+
+    public bool IsTwoPair(Hand hand)
+    {
+        return true;
+    }
+
+    public bool IsOnePair(Hand hand)
+    {
+        return true;
+    }
+
+    public bool IsHighCard(Hand hand) => true;
+
+
+}
+
+public static class Extensions
+{
+    public static int SymbolId(this Card card)
+    {
+        if (char.IsLetter(card.Symbol))
+        {
+            switch (card.Symbol)
+            {
+                case 'J': return 11;
+                case 'Q': return 12;
+                case 'K': return 13;
+                case 'A': return 1;
+                default: return -1;
+            }
+        }
+        else
+        {
+            return card.Symbol - '0';
+        }
     }
 }
---
+++ poker/RankingValidatorTest.cs
@@ -0,0 +1,39 @@
+using Xunit;
+public class RankingValidatorTest
+{
+    [Theory]
+    [InlineData("3S 4D 2S 6D 5C")]
+    public void IsStraight_should_be_true(string pokerHand)
+    {
+        // Arrange 
+        var hand = new Hand(pokerHand);
+        var validator = new RankingValidator();
+        
+        // Act & Assert
+        Assert.True(validator.IsStraight(hand));
+    }
+
+    [Theory]
+    [InlineData("2S 4S 5S 6S 7S")]
+    public void IsFlush_should_be_true(string pokerHand)
+    {
+         // Arrange 
+        var hand = new Hand(pokerHand);
+        var validator = new RankingValidator();
+        
+        // Act & Assert
+        Assert.True(validator.IsFlush(hand));
+    }
+
+    [Theory]
+    [InlineData("4H 6H 7H 8H 5H")]
+    public void IsStraightFlush_should_be_true(string pokerHand)
+    {
+         // Arrange 
+        var hand = new Hand(pokerHand);
+        var validator = new RankingValidator();
+        
+        // Act & Assert
+        Assert.True(validator.IsStraightFlush(hand));
+    }
+}
